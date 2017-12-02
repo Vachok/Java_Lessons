31e39da6cb22765408ed9533fc0086fc873ef321
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-02 15:05:34 +0300

    https://www.youtube.com/watch?v=g61oFg93V1U
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- Code as is/Lesson/Human.java
+++ Code as is/Lesson/Human.java
@@ -1,7 +1,10 @@
 package Lesson;
-
+   import Lesson.ru.vachok.www.*;
 
 class Human {
+    Car myCar;
+    Car momCar;
+    Plane myPlane;
     public static void main (String[] args){
 
     }
---
+++ Code as is/Lesson/ru/vachok/www/Plane.java
@@ -0,0 +1,4 @@
+package Lesson.ru.vachok.www;
+
+public class Plane {
+}
