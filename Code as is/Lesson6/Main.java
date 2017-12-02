9bf9f768ac43adf05f6b205a758625288c4c6010
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-02 11:44:18 +0300

    Описание типов переменных
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- Code as is/Lesson6/Main.java
+++ Code as is/Lesson6/Main.java
@@ -1,14 +1,56 @@
 package Lesson6;
 
-public class Main {
-    byte b;
-    short s;
-    int i;
-    long l;
-    float f;
-    double d;
-    boolean bool;
-    char c;
+/** Условный человек
+ *
+ */
+class Human {
+    /**
+     * Не менее 128 и не более 128
+     * Используется для экономии памяти
+     * @deprecated
+     */
+    byte b = 120;
+    /**
+     * Переменная на 2 байта. ~65500 (-32 - +32)
+     * @deprecated
+     */
+    short s = 24567;
+    /**
+     * 4 байта. для 32 бит
+     */
+    int i = 5555558;
+    /**
+     * 8 байт. для 64 бит
+     */
+    long l = 767464654;
+    /**
+     * Значение с точкой. 32 bit
+     * Необходимо узазывать f в конце, чтобы не путать с double
+     */
+    float f = 5.64f;
+    /**
+     * Значения с точкой. 64 bit
+     */
+    double d = 5.64;
+    /**
+     * Проверить правда / ложь
+     */
+    boolean bool = false;
+    /**
+     * 1 символ
+     * @deprecated
+     */
+    char c = '4';
+
+    /**
+     * Ссылочная переменная (КЛАСС)
+     * Не хранит в памяти. Только ссылается на объект
+     */
+    Human friend = new Human();
+    Human mom;
+    Human dad;
+
+    Car car = new Car();
 
     public static void main(String[] args) {
 
