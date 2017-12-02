e0073f7337aadd63c2d462716e56be36f7af7d84
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-02 10:08:56 +0300

    https://youtu.be/S1GVLezqYoE?t=2m1s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- Code as is/Lesson6/Main.java
+++ Code as is/Lesson6/Main.java
@@ -1,5 +1,18 @@
 package Lesson6;
 
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+boolean bool;
+char c;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java_Lessons
 by https://www.youtube.com/playlist?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX
+
+https://youtu.be/S1GVLezqYoE?t=2m1s
