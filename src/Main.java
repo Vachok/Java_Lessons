29f700bbe71460ac3396a7008ad448edd2f38dc2
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-03 11:37:16 +0300

    @Override
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 
 
 public class Main {
+    void useCar (Car car){
+        car.driving();
+        System.out.println(car.getNumOfSeat());
+    }
     public static void main(String[] args){
-
+        Main main = new Main();
+        main.useCar(new Car());
     }
 }
