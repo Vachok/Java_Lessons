0a5cbdc0d9d5b76d382ecd1f762d9f444f943102
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-03 15:23:59 +0300

    https://www.youtube.com/watch?v=I3t_PfbDAx8
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 
 public class Main {
+    int camelCase;
+    int myVariableWithVeryLongName;
+
+    void myMethodWithLongName(){
+
+    }
     public static void main(String[] args){
 
     }
---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private String myName;
+    private boolean blond;
+
+    public boolean isBlond(){
+        return blond;
+    }
+
+    public String getMyNameName() {
+        return setMyName();
+    }
+
+    public void setMyName( String name ) {
+        this.setMyName(); = name;
+    }
+}
