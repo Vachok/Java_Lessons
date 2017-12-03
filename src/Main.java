f38b86bd19bb740a636bb92c8f6eba9c48976b45
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-03 12:07:36 +0300

    https://www.youtube.com/watch?v=2giCFoDw2c4
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 
 public class Main {
     public static void main(String[] args){
-
+        Person person = new Person();
+        System.out.println(person.getName());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,8 @@
 public class Person {
+    private String mr = "Mr";
+    private String name = "Max";
+
+    public String getName() {
+        return mr + name;
+    }
 }
