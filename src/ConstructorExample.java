6696bfb67846159d58a5ff1e6cfa2a1f98f764e1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 09:35:43 +0300

    Выводит run
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ConstructorExample.java
+++ src/ConstructorExample.java
@@ -1,2 +1,11 @@
-public class ConstructorExample {
+public class ConstructorExample { //https://youtu.be/Se9yZGZg9-w?t=1m
+     ConstructorExample(){ //не может быть статик, абстракт, финал
+        System.out.println("run");
+    }
+}
+
+class Run {
+    public static void main (String[] args) {
+        ConstructorExample constructorExample = new ConstructorExample();
+    }
 }
