29fb3b2d2af789fea3f4677b29b712829885041c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 18:27:34 +0300

    Java_Lessons-31 25: Ключевое слово static и инициализация классов https://youtu.be/xWSZZwDpcGU?t=1m38s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/StaticExmpl.java
+++ src/StaticExmpl.java
@@ -1,2 +1,17 @@
 public class StaticExmpl {
+    static int i;
+    int i;
+    static {
+        System.out.println("static initializer");
+    }
+    {
+        System.out.println("initializator");
+    }
+    StaticExmpl() {
+        System.out.println("Constructor");
+    }
+
+    public static void main( String[] args ) {
+
+    }
 }
