ff804933b58e88d1e054b3147da47d5ce8f0c972
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 09:48:33 +0300

    run 5 https://youtu.be/Se9yZGZg9-w?t=7m23s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ConstructorExample.java
+++ src/ConstructorExample.java
@@ -14,11 +14,15 @@ public class ConstructorExample {
         System.out.println("run");
         System.out.println(hello);
     }
+    ConstructorExample(int i) { //не может быть статик, абстракт, финал
+        this.i = i;
+        System.out.println("run");
+    }
 }
 
 class Run {
     public static void main (String[] args) {
-        ConstructorExample constructorExample = new ConstructorExample("jhghjgg");
+        ConstructorExample constructorExample = new ConstructorExample(5);
         System.out.println(constructorExample.i);
     }
 }
