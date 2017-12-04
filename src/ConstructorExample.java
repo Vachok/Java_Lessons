07519cbed7857943199c889f064b5933ffc6ae5a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 09:43:58 +0300

    https://youtu.be/Se9yZGZg9-w?t=5m28s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ConstructorExample.java
+++ src/ConstructorExample.java
@@ -1,6 +1,6 @@
 public class ConstructorExample {
     int i;
-    ConstructorExample(String hello, int i) { //не может быть статик, абстракт, финал
+    void ConstructorExample(String hello, int i) { //не может быть статик, абстракт, финал
         this.i = i;
         System.out.println("run");
         System.out.println(hello);
@@ -9,7 +9,7 @@ public class ConstructorExample {
 
 class Run {
     public static void main (String[] args) {
-        ConstructorExample constructorExample = new ConstructorExample("hello", 5);
-        System.out.println(constructorExample.i); //https://youtu.be/Se9yZGZg9-w?t=4m37s
+        ConstructorExample constructorExample = new ConstructorExample();
+        System.out.println(constructorExample.i);
     }
 }
