3a3bbcd7cfa97da5a76bec740d6530b29f8dc578
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 22:02:15 +0300

    Java_Lessons-31 25: Ключевое слово static и инициализация классов https://youtu.be/xWSZZwDpcGU?t=13m58s static initializer static method initializator secondInitializator Constructor initializator secondInitializator Constructor
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/StaticExmpl.java
+++ src/StaticExmpl.java
@@ -1,27 +1,28 @@
 public class StaticExmpl {
-    static int i; //Шаг1 инициализация переменных
-    static int g = 6; //static
-    int j;
-    int b = 6; //step3
+    static int i;
+    static int g = 6;
+    int b = 6;
+
     static {
-        System.out.println("static initializer"); //шаг2 инициализаторы
+        System.out.println("static initializer"); //шаг1
     }
+
     {
-        System.out.println("initializator"); //шаг 4 (не статический иниц.
+        System.out.println("initializator");
     }
-    {
-        System.out.println("secondInitializator");
+        {
+            System.out.println("secondInitializator");
+        }
+        StaticExmpl() {
+        System.out.println("Constructor");
     }
-    StaticExmpl() {
-        System.out.println("Constructor"); //step5
-    }
-    static void staticMethod() {
-        System.out.println("static method");
+        static void staticMethod () {
+        System.out.println("static method"); //step2
     }
 
     public static void main( String[] args ) {
-        System.out.println(StaticExmpl.g);
+        staticMethod();
         new StaticExmpl(); //причина step3
-        new StaticExmpl(); //причина step3 новые СтатикКлассы
+        new StaticExmpl(); //
     }
 }
