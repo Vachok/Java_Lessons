cb2af325bfee50f95cac7ff18720465c7b50739b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 11:29:31 +0300

    То, что происходит в enum https://youtu.be/ll14SKsQScE?t=5m4s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/EnumsExample.java
+++ src/EnumsExample.java
@@ -1,6 +1,13 @@
 public class EnumsExample {
     enum CoffeSize {SMALL, MEDIUM, BIG}
 
+/*    То, что происходит в enum
+class CoffeSize {
+        public static final EnumsExample.CoffeSize SMALL = new EnumsExample.CoffeSize(1);
+        CoffeSize(int i);
+
+    }*/
+
     public static void main(String[] args) {
         CoffeSize coffeSize = CoffeSize.BIG;
         System.out.println(coffeSize);
