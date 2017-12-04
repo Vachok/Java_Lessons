8f6c4ae5e283c677b3902e831742b72193e21907
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 10:06:56 +0300

    массив из массивов https://youtu.be/v7yZ9okDsS4?t=2m16s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -2,5 +2,5 @@ public class ArrayExample {
     int[] arr = {5,4,3};
     boolean[] booleansArray = {true, false};
     double[] doubles = {5.234, 5.234};
-    int array[];
+    int[][] array = {{5,3},{5,2}};
 }
