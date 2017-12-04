e7ee36f025f38a2d2038ae52a184d548b3bf9ab1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 11:46:53 +0300

    Почему мой код не работает!? https://youtu.be/ll14SKsQScE?t=8m52s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/EnumsExample.java
+++ src/EnumsExample.java
@@ -1,8 +1,16 @@
 public class EnumsExample {
-    enum CoffeSize {SMALL, MEDIUM, BIG} //создание перечисления
+    enum CoffeSize {SMALL(100), MEDIUM(200), BIG(300)} //создание перечисления
+        int ml;
+        CoffeSize(int ml) {
+            this.ml = ml;
+            }
+            int getMl() {
+                return ml;
+            }
+        }
 
     public static void main(String[] args) {
         CoffeSize coffeSize = CoffeSize.BIG;
         System.out.println(coffeSize); //выводим BIG
+        System.out.println(coffeSize.getMl());
     }
-}
