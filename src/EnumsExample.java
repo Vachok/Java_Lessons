fa23d253cad1fc7690fa88c306555179632669b5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 12:36:03 +0300

    BIG 300 A https://youtu.be/ll14SKsQScE?t=11m15s Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/EnumsExample.java
+++ src/EnumsExample.java
@@ -1,14 +1,17 @@
 public class EnumsExample {
     enum CoffeSize {
         SMALL(100), MEDIUM(200), BIG(300); //создание перечисления
+        String coffeClass = "A";
         int ml;
 
         CoffeSize( int ml ) {
             this.ml = ml;
         }
-
         int getMl() {
             return ml;
+        }
+        String getCoffeClass() {
+            return coffeClass;
         }
     }
 
@@ -16,5 +19,6 @@ public class EnumsExample {
         CoffeSize coffeSize = CoffeSize.BIG;
         System.out.println(coffeSize); //выводим BIG
         System.out.println(coffeSize.getMl());
+        System.out.println(coffeSize.getCoffeClass());
     }
 }
