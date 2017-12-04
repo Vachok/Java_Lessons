c8f6d53e4a3d0a34ab892a5f78c2e3aabc4e7a81
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 13:04:38 +0300

    11 3.9 https://youtu.be/OE6jUYt8O4Q?t=2m46s Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Overload.java
+++ src/Overload.java
@@ -1,2 +1,14 @@
 public class Overload {
+    int addTwoDigits(int a, int b) {
+        return a + b;
+    }
+    double addTwoDigits(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Overload overload = new Overload();
+        System.out.println(overload.addTwoDigits(5,6));
+        System.out.println(overload.addTwoDigits(2.5, 1.4));
+    }
 }
