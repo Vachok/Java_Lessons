19ae5d81739e30570a24467dd61dee4db1aad8cb
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 15:50:34 +0300

    Java_Lessons-30 24: Overriding - переопределение методов https://youtu.be/2TlE11Bfzi0?t=50s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/OverrideExample.java
+++ src/OverrideExample.java
@@ -1,2 +1,16 @@
+class Parent{
+    int method() {
+        return i;
+    }
+}
+
+class Child extends Parent {
+    @Override
+    int method() {
+        return 2;
+    }
+}
+
 public class OverrideExample {
+
 }
