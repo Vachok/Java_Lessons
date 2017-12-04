f24c06726e987dcca2347c62680eb441a5f09109
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 09:46:48 +0300

    run jhghjgg 0 https://youtu.be/Se9yZGZg9-w?t=6m53s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ConstructorExample.java
+++ src/ConstructorExample.java
@@ -1,6 +1,15 @@
 public class ConstructorExample {
     int i;
-    void ConstructorExample(String hello, int i) { //не может быть статик, абстракт, финал
+    ConstructorExample(String hello, int i) { //не может быть статик, абстракт, финал
+        this.i = i;
+        System.out.println("run");
+        System.out.println(hello);
+    }
+    ConstructorExample() { //не может быть статик, абстракт, финал
+        this.i = i;
+        System.out.println("run");
+    }
+    ConstructorExample(String hello) { //не может быть статик, абстракт, финал
         this.i = i;
         System.out.println("run");
         System.out.println(hello);
@@ -9,8 +18,7 @@ public class ConstructorExample {
 
 class Run {
     public static void main (String[] args) {
-        ConstructorExample constructorExample = new ConstructorExample();
-        constructorExample.ConstructorExample("hi", 2);
+        ConstructorExample constructorExample = new ConstructorExample("jhghjgg");
         System.out.println(constructorExample.i);
     }
 }
