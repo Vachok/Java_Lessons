75f92a472d377e3b2195a1960bb8336c98883e08
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 09:37:17 +0300

    run hello
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ConstructorExample.java
+++ src/ConstructorExample.java
@@ -1,11 +1,12 @@
 public class ConstructorExample { //https://youtu.be/Se9yZGZg9-w?t=1m
-     ConstructorExample(){ //не может быть статик, абстракт, финал
+     ConstructorExample(String hello) { //не может быть статик, абстракт, финал
         System.out.println("run");
+        System.out.println(hello);
     }
 }
 
 class Run {
     public static void main (String[] args) {
-        ConstructorExample constructorExample = new ConstructorExample();
+        ConstructorExample constructorExample = new ConstructorExample("hello");
     }
 }
