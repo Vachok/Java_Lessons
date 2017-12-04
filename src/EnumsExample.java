416a8e2700a2287a38b90d09e9f1238f50aa57ba
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-04 12:18:31 +0300

    BIG 300 https://youtu.be/ll14SKsQScE?t=8m54s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/EnumsExample.java
+++ src/EnumsExample.java
@@ -1,16 +1,20 @@
 public class EnumsExample {
-    enum CoffeSize { SMALL(100), MEDIUM(200), BIG(300) //создание перечисления
+    enum CoffeSize {
+        SMALL(100), MEDIUM(200), BIG(300); //создание перечисления
         int ml;
-        CoffeSize(int ml) {
+
+        CoffeSize( int ml ) {
             this.ml = ml;
-            }
-            int getMl() {
-                return ml;
-            }
         }
 
-    public static void main(String[] args) {
+        int getMl() {
+            return ml;
+        }
+    }
+
+    public static void main( String[] args ) {
         CoffeSize coffeSize = CoffeSize.BIG;
         System.out.println(coffeSize); //выводим BIG
         System.out.println(coffeSize.getMl());
     }
+}
