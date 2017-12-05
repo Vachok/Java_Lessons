3a103378d622801c8c4cc00693995791f9e132ac
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 10:23:05 +0300

    Java_Lessons-36 26: Блоки инициализации итоговый алгоритм
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/InitExample.java
+++ src/InitExample.java
@@ -1,32 +1,32 @@
-class Parrent {
-    static {
-        System.out.println("static parrent block"); //https://youtu.be/oXtAi0Z2IkE?t=6m34s
+class Parrent { //2. выполняется его статика
+    static {//3
+        System.out.println("static parrent block");
     }
-    Parrent() {
+    Parrent() { //6. т.к. мы пришли из extends
         System.out.println("Parrent cons");
     }
 }
 
-public class InitExample extends Parrent {
+public class InitExample extends Parrent { //1 из запуска, идём сюда. сморим в extends и идём в Parrent
     static {
-        System.out.println("static init block");
+        System.out.println("static init block");//4. т.к. static
+    }
+    { //класс Parrent закончился, продолжаем
+        System.out.println("init block1"); //7
     }
     {
-        System.out.println("init block1");
-    }
-    {
-        System.out.println("init block2");
+        System.out.println("init block2"); //8
     }
     InitExample() {
-        System.out.println("constructor");
+        System.out.println("constructor"); //10
     }
         public static void main( String[] args ) {
-        new InitExample();
+        new InitExample(); //0.
     }
     {
-        System.out.println("init block3");
+        System.out.println("init block3"); //9
     }
     static {
-        System.out.println("static init block2");
+        System.out.println("static init block2");//5.
     }
 }
