032ad694c6c8669b99d02e618eb0c4ce8427d7d6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 15:59:33 +0300

    Java_Lessons-38 28: Обертки https://youtu.be/ZHJLt6sadGs?t=13m11s 6 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AutoBox {
     public static void main( String[] args ) {
         Integer i = 5; // = new Integer(5);
-        i++;
+        i++; // = int tempVar = i.tempValue(); tempVar++; i = new Integer(tempVar); ++ добавлен в java5
         System.out.println(i);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3); // обычный int так не пойдёт
+        for (int k : list) {
+            System.out.println(k);
+        }
+
     }
 }
