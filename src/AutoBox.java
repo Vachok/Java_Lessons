059fd1a71f3f5cdd866e7e79f15cd19adea8a72c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 16:17:05 +0300

    Java_Lessons-38 28: Обертки https://youtu.be/ZHJLt6sadGs?t=20m16s пустой вывод
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -4,9 +4,7 @@ import java.util.List;
 public class AutoBox {
     public static void main( String[] args ) {
         String s = "string"; //хранятся в пуле
-        String s2 = "string";
-        Object o1 = new Object();
-        Object o2 = o1;
+        String s2 = new String("string");
         if(s == s2) { //не рекомендуется так. т.к. область памяти String *может* быть разной
             System.out.println("==");
         }
