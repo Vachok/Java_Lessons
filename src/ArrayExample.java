9bb296aa3291b4d4b36c53bb233601e7fce6d79f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 11:51:54 +0300

    Java_Lessons-37 27: инициализация массивов null null null
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -3,7 +3,7 @@ public class ArrayExample {
         Object[] i = new Object[5]; //5 - massive size слово new - выделяет память
         int[] j = new int[3];
         for (int k = 0; k < j.length; k++) {
-            System.out.println(j[k]);
+            System.out.println(i[k]);
         }
     }
 }
