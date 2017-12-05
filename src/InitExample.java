e0c4e5757359fe253856d7ced2a1af259b37e8b3
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 09:52:00 +0300

    Java_Lessons-36 26: Блоки инициализации https://youtu.be/oXtAi0Z2IkE?t=1m12s static init block
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/InitExample.java
+++ src/InitExample.java
@@ -1,7 +1,10 @@
 public class InitExample {
+    static {
+        System.out.println("static init block");
+    } //статический код инициализации. запуск 1
     {
-        System.out.println();
-    } //обычный код инициализации
+        System.out.println("init block");
+    } //обычный код инициализации. запуск при создании экземпляра
     public static void main( String[] args ) {
 
     }
