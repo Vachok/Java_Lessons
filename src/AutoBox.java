81837ad955ef421994f81202586efa58cbf0c1b5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 16:18:17 +0300

    Java_Lessons-38 28: Обертки == (equals)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -5,7 +5,7 @@ public class AutoBox {
     public static void main( String[] args ) {
         String s = "string"; //хранятся в пуле
         String s2 = new String("string");
-        if(s == s2) { //не рекомендуется так. т.к. область памяти String *может* быть разной
+        if(s.equals(s2)) {
             System.out.println("==");
         }
     }
