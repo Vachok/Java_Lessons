3b7fda18d5e2d18217ffe9ab207c7b30149216a2
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 11:51:03 +0300

    Java_Lessons-37 27: инициализация массивов https://youtu.be/i_IiGj65bJM?t=4m58s 0 0 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,5 +1,9 @@
 public class ArrayExample {
     public static void main( String[] args ) {
-
+        Object[] i = new Object[5]; //5 - massive size слово new - выделяет память
+        int[] j = new int[3];
+        for (int k = 0; k < j.length; k++) {
+            System.out.println(j[k]);
+        }
     }
 }
