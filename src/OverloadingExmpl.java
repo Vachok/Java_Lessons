5ec57c40747ad2fd3c8054bd4f2034ff39db86d3
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 21:32:21 +0300

    Java_Lessons-39 29: перегрузка методов 2 long
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/OverloadingExmpl.java
+++ src/OverloadingExmpl.java
@@ -2,10 +2,17 @@ public class OverloadingExmpl {
     public static void main( String[] args ) {
         OverloadingExmpl overloadingExmpl = new OverloadingExmpl();
         short b = 5;
-        overloadingExmpl.method((int)(b));
+        short c = 5;
+        overloadingExmpl.method(b, c);
     }
-
-    void method(Integer i) {
-        System.out.println("Integer"); //не запустится
+// 3 основных типа
+    void method(long i , long c) { //1 расширение short -> long (exmpl)
+        System.out.println("long");
+    }
+    void method(Short i, Short c) { //2 autoboxing
+        System.out.println("Short");
+    }
+    void method(short... i) { //3 massive
+        System.out.println("Short");
     }
 }
