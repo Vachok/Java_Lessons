0c22d1242bf08c3a433dfadebf564b470eee4f9f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 16:20:41 +0300

    Java_Lessons-38 28: Обертки https://youtu.be/ZHJLt6sadGs?t=21m27s equals
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -3,10 +3,10 @@ import java.util.List;
 
 public class AutoBox {
     public static void main( String[] args ) {
-        String s = "string"; //хранятся в пуле
-        String s2 = new String("string");
-        if(s.equals(s2)) {
-            System.out.println("==");
+        Integer i = 5;
+        Integer j = 5;
+        if (i.equals(j)) {
+            System.out.println("equals");
         }
     }
 }
