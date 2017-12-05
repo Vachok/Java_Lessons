ad8106ddce8207e5bf04ffb656c8b18b28ce3f86
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 14:52:15 +0300

    Java_Lessons-38 28: Обертки
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java_Lessons
 by https://www.youtube.com/playlist?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX
+
+https://youtu.be/ZHJLt6sadGs?t=6m59s
--- src/AutoBox.java
+++ src/AutoBox.java
@@ -1,26 +1,8 @@
 public class AutoBox {
     public static void main( String[] args ) {
-//основные примитивные переменные (в отличие от ссылочных - они хранят только свои значения)
-        int i;
-        byte b;
-        short s;
-        long l;
-        boolean bl;
-        char ch;
-        float f;
-        double d;
-//Обёртки
-        Byte bt;
-        Short shrt;
-        Integer integer;
-        Long lng;
-        Boolean bln;
-        Character chr;
-        Float fl;
-        Double doubl;
-
-        List<Integer> list;
-//      int j = "5" не сработает. сработает через обёртку:
-        int j = new Integer("5")
+        Integer i = new Integer(5);
+        Integer j = new Integer("6");
+        int k = Integer.parseInt("2");
+        Integer.valueOf("3")
     }
 }
