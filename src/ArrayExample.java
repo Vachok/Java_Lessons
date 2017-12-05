ce3c59560fa2007f5c27ea93cef8d1359babb7de
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 12:16:05 +0300

    Java_Lessons-37 27: инициализация массивов https://youtu.be/i_IiGj65bJM?t=13m47s 1 2 3 4
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,5 +1,8 @@
 public class ArrayExample {
     public static void main( String[] args ) {
-        int[][] i = {{1,3},{5,3,}};
+        int[] i = {1,2,3,4};
+        for (int j = 0; j < i.length ; j++) {
+            System.out.println(i[j]);
+        }
 }
 }
