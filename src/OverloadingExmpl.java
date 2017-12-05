78d1610fd83f219f018e65f812b68751e16444e4
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 21:25:27 +0300

    Java_Lessons-39 29: перегрузка методов 2 https://youtu.be/o37qhweFZBM?t=14m34s Integer
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/OverloadingExmpl.java
+++ src/OverloadingExmpl.java
@@ -2,7 +2,7 @@ public class OverloadingExmpl {
     public static void main( String[] args ) {
         OverloadingExmpl overloadingExmpl = new OverloadingExmpl();
         short b = 5;
-        overloadingExmpl.method(b);
+        overloadingExmpl.method((int)(b));
     }
 
     void method(Integer i) {
