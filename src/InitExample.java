6a95bf22ccc592ec0e58fbd5c7408e89341cb06f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 10:09:16 +0300

    Java_Lessons-36 26: Блоки инициализации Вывод по-шагам https://youtu.be/oXtAi0Z2IkE?t=4m34s static init block Parrent cons init block constructor
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/InitExample.java
+++ src/InitExample.java
@@ -1,15 +1,21 @@
-public class InitExample {
+class Parrent {
+    Parrent() {
+        System.out.println("Parrent cons"); //4.вывод
+    }
+}
+
+public class InitExample extends Parrent { //1.Обратились в класс
     static {
-        System.out.println("static init block");
-    } //статический код инициализации. запуск при первом обращении
+        System.out.println("static init block"); //1,1.вывод
+    }
     {
-        System.out.println("init block");
-    } //обычный код инициализации. запуск при создании экземпляра
-    InitExample() {
-        //super(); он есть по-умолчанию
-        System.out.println("constructor");
+        System.out.println("init block");//5.вывод
+    }
+    InitExample() { //2.после static попадаем сюда, в конструктор
+        super(); //3.отправляет вначало. в класс Parrent
+        System.out.println("constructor"); //6.вывод
     }
         public static void main( String[] args ) {
-        new InitExample();
+        new InitExample();//0.запуск
     }
 }
