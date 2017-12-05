ba7e85f83fe51ee080bea7b5071e51fa52665eca
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 09:24:38 +0300

    Java_Lessons-30 24: Overriding - переопределение методов
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/StaticExample.java
+++ src/StaticExample.java
@@ -1,27 +1,27 @@
 public class StaticExample {
-    static StaticExample staticExample = new StaticExample();
-    static int n = 4;
-    static int i = returnIntStatic();
+    static StaticExample staticExample = new StaticExample(); //записываем в static class конструктор. может запустить обычные поля ДО статик
+    static int n = 4; //стaтика инициализируется первая. по-очереди
+    static int i = returnIntStatic(); //но в этом примере статичный StaticExample инициализирует конструктор
 
     static int k = 3;
-    int j = returnInt();
+    int j = returnInt(); //после иниц. статики, запускается метод
     static {
-        System.out.println("static");
+        System.out.println("static"); //7. после статичных полей, иниц. статичный конструктор
     }
     {
-        System.out.println("clean");
+        System.out.println("clean"); //2. после static-конструктора, ещё раз. 8
     }
     StaticExample() {
-        System.out.println("constructor");
+        System.out.println("constructor"); //3 и ещё раз 9
     }
     static int returnIntStatic() {
-        System.out.println(n);
-        System.out.println(k);
-        System.out.println("return static int");
+        System.out.println(n); //4
+        System.out.println(k); //5
+        System.out.println("return static int"); //6
         return 1;
     }
     int returnInt() {
-        System.out.println("return int");
+        System.out.println("return int"); //1
         return 2;
     }
 
@@ -29,3 +29,16 @@ public class StaticExample {
         new StaticExample();
     }
 }
+/* Итоговый вывод на экран:
+return int
+        clean
+        constructor
+        4
+        0
+        return static int
+static
+return int
+        clean
+        constructor
+https://youtu.be/xWSZZwDpcGU?t=18m44s
+        */
