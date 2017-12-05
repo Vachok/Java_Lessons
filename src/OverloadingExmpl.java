57d9371156d47955406b37946f5837899087008c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 19:44:43 +0300

    Java_Lessons-39 29: перегрузка методов 2 https://youtu.be/o37qhweFZBM?t=7m20s byte
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/OverloadingExmpl.java
+++ src/OverloadingExmpl.java
@@ -1,2 +1,37 @@
 public class OverloadingExmpl {
+    public static void main( String[] args ) {
+        OverloadingExmpl overloadingExmpl = new OverloadingExmpl();
+        short b = 5;
+        overloadingExmpl.method((byte)b);
+
+    }
+// Что стартует первым?
+
+    void method(int i) {
+        System.out.println("int");
+    }
+    void method(long i) {
+        System.out.println("long");
+    }
+    void method(Integer i) {
+        System.out.println("Integer");
+    }
+    void method(double i) {
+        System.out.println("double");
+    }
+    void method(short... i) {
+        System.out.println("byte...");
+    }
+    void method(Short i) {
+        System.out.println("Byte");
+    }
+    void method(Object i) {
+        System.out.println("Object");
+    }
+    void method(Object... i) {
+        System.out.println("Object");
+    }
+    void method(byte i) {
+        System.out.println("byte");
+    }
 }
