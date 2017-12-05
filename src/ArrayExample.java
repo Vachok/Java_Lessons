a3c962cb34f9d12360f893daa327fd37a22f2e7b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 12:11:54 +0300

    Java_Lessons-37 27: инициализация массивов можно так
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,10 +1,5 @@
 public class ArrayExample {
     public static void main( String[] args ) {
-        method(new int[]{1,4,2});
-    }
-    static void method(int[] j) {
-        for (int i = 0; i < j.length; i++) {
-            System.out.println(j[i]);
-        }
-    }
+        int[][] i = new int[][]{}{1,3},{5,3,}}
+}
 }
