ec8ba15ff63dbaefa0240f6c619365a1549b2a81
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 14:46:04 +0300

    Java_Lessons-38 28: Обертки https://youtu.be/ZHJLt6sadGs?t=5m12s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -1,5 +1,29 @@
 public class AutoBox {
     public static void main( String[] args ) {
+//основные примитивные переменные (в отличие от ссылочных - они хранят только свои значения)
+        int i;
+        byte b;
+        short s;
+        long l;
+        boolean bl;
+        char ch;
+        float f;
+        double d;
+//Обёртки
+        Byte bt;
+        Short shrt;
+        Integer integer;
+        Long lng;
+        Boolean bln;
+        Character chr;
+        Float fl;
+        Double doubl;
 
+        List<Integer> list;
+
+    }
+
+    Object method() {
+        return 1;// = new Integer(1;) обёртка приводит этот Object к Integer
     }
 }
