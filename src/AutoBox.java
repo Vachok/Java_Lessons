3b53dcd53807a296ce20af40d2c529bc22eacaa6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-05 15:47:33 +0300

    Java_Lessons-38 28: Обертки https://youtu.be/ZHJLt6sadGs?t=10m8s 6
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/AutoBox.java
+++ src/AutoBox.java
@@ -1,9 +1,7 @@
 public class AutoBox {
     public static void main( String[] args ) {
-        Integer i = new Integer(5);
-        Integer j = new Integer("6");
-        int k = Integer.parseInt("2");
-        int g =  Integer.valueOf("3"); //парсит строку
-        short s = i.shortValue(); //возвращает переконвертированное значение
+        Integer i = 5; // = new Integer(5);
+        i++;
+        System.out.println(i);
     }
 }
