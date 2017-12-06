da3b207275040603aba9cb145294e95bebaa2f4a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 19:39:05 +0300

    Java_Lessons-47 37: Оператор ветвления if и else bla
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         int b = 6;
         int c = 4;
         int d = 5;
-        if (a == b)
-            if(c == d)
-            System.out.println("a > b");
+        if (a == b) {
+            if (c == d) {
+                System.out.println("a > b");
+            }
+        }
         System.out.println("bla");
     }
 }
