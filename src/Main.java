069cc755e2c37781b11dcd97e13b1c24c472db95
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 10:22:58 +0300

    Java_Lessons-44 Java 34: Garbage Collector https://youtu.be/-ayMplONmkI?t=17m7s total memory before: 100663296 free memory before: 96172304 total memory after: 100663296 free memory after: 96172304 total memory after gc: 8388608 free memory after gc: 7179952
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.Date;
 
 public class Main {
     public static void main( String[] args ) {
-        Date dt = method();
-        dt = null;
-        System.gc(); // "предлагает" вирт.машине запустить Garbage collector
-        method2();
+        method();
     }
 
-    private static Date method() {
-        Date date;
-        date = new Date();
-        Date date2 = date;
-        System.out.println(date);
-        return date;
+    private static void method() {
+        Runtime runTime = Runtime.getRuntime();
+        System.out.println("total memory before: " + runTime.totalMemory());
+        System.out.println("free memory before: " + runTime.freeMemory());
+        for (long i = 0; i < 2000000000; i++) ;
+        { // создание кучи объектов
+            Date date = new Date(); // выделение памяти
+            date = null; // помечаем на удаление
+        }
+        System.out.println("total memory after: " + runTime.totalMemory());
+        System.out.println("free memory after: " + runTime.freeMemory());
+        System.gc();
+        System.out.println("total memory after gc: " + runTime.totalMemory());
+        System.out.println("free memory after gc: " + runTime.freeMemory());
     }
 
     private static void method2() {
