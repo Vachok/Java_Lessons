ca1c7ee9efabb16bcf6abc99923992cdd858ec84
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 16:22:35 +0300

    Java_Lessons-47 37: Оператор ветвления if и else https://youtu.be/AzbyMP6qXo0?t=1m47s a > b
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 
 public class Main {
     public static void main( String[] args ) {
-
+        int a = 10;
+        int b = 6;
+        if (a > b) {
+            System.out.println("a > b");
+        }
     }
 }
