5229c5916ba09f9512d99c0287b5cf5d31aa3a8c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:22:46 +0300

    Java_Lessons-45 35: операторы Java true
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main extends Object {
     public static void main( String[] args ) {
         Main main = new Main();
-        System.out.println(main instanceof Object);
+        System.out.println(main instanceof Main);
     }
 }
