151bad5b810ab2f76b47dc544d03179a6d52ac11
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:40:28 +0300

    Java_Lessons-45 35: операторы Java 1
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Main extends Object {
     public static void main( String[] args ) {
-        int a = (5 > 6) ? 1 : 2;
+        int a = (7 > 6) ? 1 : 2;
         System.out.println(a);
     }
 }
