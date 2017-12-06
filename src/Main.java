7585706c750e8cd3a04da1fc370d7e169f752f47
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:17:26 +0300

    Java_Lessons-45 35: операторы Java false
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         int a = 5;
-        double b = 5.0;
+        double b = 5.1;
         System.out.println(a == b);
     }
 }
