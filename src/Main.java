bc6314bee328dfe2ba6073414679f0fca7e89e8a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 09:30:19 +0300

    Java_Lessons-44 Java 34: Garbage Collector https://youtu.be/-ayMplONmkI?t=5m59s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-
+import java.util.Date;
 
 public class Main {
     public static void main( String[] args ) {
+        Date dt = method()
+        //после выполнения, объект может быть удалён
+        method();
+        method2();
+    }
 
+    private static Date method() {
+        // Date date; - так объект НЕ создаётся. это только объявление
+        // new Date(); - так объект создан, но потерян. нет ссылки
+        Date date; //создание ссылки на объект в памяти
+        date = new Date(); //создание объекта в памяти
+        Date date2 = date;
+        System.out.println(date);
+        return date;
+    }
+
+    private static void method2() {
     }
 }
