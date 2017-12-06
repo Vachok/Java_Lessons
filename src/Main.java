ec13cc8de0444c4e0618a92293902cce028b3953
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:19:21 +0300

    Java_Lessons-45 35: операторы Java https://youtu.be/m_DANftwUxU?t=8m44s false
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 
 public class Main {
     public static void main( String[] args ) {
-        int a = 5;
-        double b = 5.1;
-        System.out.println(a != b);
+        Object o = new Object();
+        Object o2 = new Object();
+        System.out.println(o == o2);
     }
 }
