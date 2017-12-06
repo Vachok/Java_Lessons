919287041e4501bbefbf1a2f8c2b697f35cb911c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:45:03 +0300

    Java_Lessons-45 35: операторы Java false
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class Main extends Object {
     public static void main( String[] args ) {
-        System.out.println(5 > 4 && 6 > 5);// если оба верны
+        System.out.println(5 > 4 && 4 > 5);
     }
 }
