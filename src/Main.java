61635c42fa919affc0f32034cecd018f955bd6ad
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 16:15:44 +0300

    Java_Lessons-46  36: метод main и передаваемые в него рамаметры
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
 
 public class Main {
-    public static void main( String[] args ) {
-
+    public static void main( String/*набор строк*/[]/*массив*/ myArgs/*метод выводит параметры*/ ) {
+        for (String s : myArgs) { //https://youtu.be/u2LxzFGdkmw
+            System.out.println(s);
     }
 }
