5d1de959c0cdbda8ebdfff64c2346dc5e2284d61
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:26:37 +0300

    Java_Lessons-45 35: операторы Java https://youtu.be/m_DANftwUxU?t=12m24s bla yohoho
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Main extends Object {
     public static void main( String[] args ) {
-        Main main = new Main();
-        System.out.println(main instanceof List);
+        String s = "bla";
+        System.out.println(s + " yohoho");
     }
 }
