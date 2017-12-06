4fd03bafc13027badfc31952ebc790d121292092
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 23:36:16 +0300

    Java_Lessons-50 38: Мало кто знает как устроен оператор ветвления switch 3 5 6 default https://youtu.be/oKcXRgnD76M?t=7m4s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,20 @@ public class Main {
 
 
     public static void main( String[] myArgs ) {
-        int i = 10;
+        int i = 3;
         switch (i) {
             case 1:
                 System.out.println(1);
-                break;
             case 2:
                 System.out.println(2);
-                break;
             case 3:
                 System.out.println(3);
-                break;
             case 5:
                 System.out.println(5);
-                break;
             case 6:
                 System.out.println(6);
-                break;
             default:
                 System.out.println("default");
-                break;
         }
     }
 }
