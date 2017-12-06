aea2761f92b64be7a504bc128d9c7d1121e1220e
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:57:08 +0300

    Java_Lessons-45 35: операторы Java true
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import java.util.List;
 
 public class Main extends Object {
     public static void main( String[] args ) {
-        System.out.println(3 > 4 && returnBool()); //
-    }
-
-    private static boolean returnBool() {
-        System.out.println("returnBoolean");
-        return true;
+        System.out.println(!(3 > 4)); //
     }
 }
