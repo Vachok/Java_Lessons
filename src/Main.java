d15d34bc305515df921b1f7e1209cc5a4c64b710
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:23:51 +0300

    Java_Lessons-45 35: операторы Java https://youtu.be/m_DANftwUxU?t=11m13s false
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
+import java.util.List;
 
 public class Main extends Object {
     public static void main( String[] args ) {
         Main main = new Main();
-        System.out.println(main instanceof Main);
+        System.out.println(main instanceof List);
     }
 }
