8c3df75542e220c2150f72eaadb6c64ad9b72a7f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:28:54 +0300

    Java_Lessons-45 35: операторы Java https://youtu.be/m_DANftwUxU?t=13m46s 56
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.List;
 public class Main extends Object {
     public static void main( String[] args ) {
         String s = "bla";
-        System.out.println("" + 5);
+        System.out.println("" + 5 + 6); // преобраз. в String
     }
 }
