3f89a90ad1f22a5c04d47ada8f66784a79112318
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 12:45:45 +0300

    Java_Lessons-45 35: операторы Java https://youtu.be/m_DANftwUxU?t=6m12s false
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 
 public class Main {
     public static void main( String[] args ) {
-        int a = 5;
-        a *= 6;
-        System.out.println(a);
+        System.out.println(5 > 6);
     }
 }
