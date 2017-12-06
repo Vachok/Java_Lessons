fbf2a7ddf609f1b65ad846d680b3400aa62addd7
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-06 13:34:22 +0300

    Java_Lessons-45 35: операторы Java 12
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.util.List;
 public class Main extends Object {
     public static void main( String[] args ) {
         int a = 5;
-        ++a;
-        System.out.println(a);
-        --a;
-        System.out.println(a);
+        System.out.println(a++ + ++a);
     }
 }
