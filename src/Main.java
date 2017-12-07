aefe5859ff1d7e34e1e8ed75dd49548fb50ca7fd
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:09:55 +0300

    последняя секция выполняется после всех операторов 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; ) {
             System.out.println(arr[i]);
+            i++;
         }
     }
 }
