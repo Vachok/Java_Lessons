12868e04938e460ed20cd6910de14f911cdaf3b6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:13:47 +0300

    операторы тоже не работают
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        for (int i = 0, j = 3, double qwe = 123;
+        for (int i = 0, j = 3, System.out.print("bla");
         i<arr.length ){
             System.out.println(arr[i]);
             i++;
