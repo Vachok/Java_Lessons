29ba732a2540945399f454b041322e8b29767f34
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:03:10 +0300

    (2)...
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        for (; true; ) {
+        for (; 5 < 6; ) {
             System.out.println(arr[1]);
         }
     }
