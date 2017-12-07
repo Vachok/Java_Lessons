e3ee5f91ac459b6daf3993e07233c98ef1103f8a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 15:04:52 +0300

    abcdef https://youtu.be/d_oou6CAz5U?t=5m35s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@
 
 public class Main {
     public static void main( String[] args ) {
-        String s = new String("abc");
-        String s2 = s.concat("def");
-        System.out.println(s);
-        System.out.println(s2);
-        StringBuilder stringBuilder;
-        StringBuffer stringBuffer; //синхронизирован. для многопотоков
+        StringBuilder stringBuilder = new StringBuilder("abc");
+        stringBuilder.append("def");
+        System.out.println(stringBuilder);
     }
 }
