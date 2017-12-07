3c870b9cc5d358a64f8161470e5e11b30ed0c0ad
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 14:42:23 +0300

    Механизм Assertions - добавить -ea в VM Options Exception in thread "main" java.lang.AssertionError 	at Main.myMethod(Main.java:9) 	at Main.main(Main.java:5)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -10,3 +10,8 @@ public class Main {
         System.out.println(a);
     }
 }
+
+//    Механизм Assertions - добавить -ea в VM Options
+//        Exception in thread "main" java.lang.AssertionError
+//        at Main.myMethod(Main.java:9)
+//        at Main.main(Main.java:5)
