f123a27cf9603bc69408117d11e1919a34f5238c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:20:16 +0300

    hello this is first state 1 end 2 end 3 end
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        for (System.out.println("hello this is first state"); i < arr.length; System.out.println("end") , i++) {
+        for (System.out.println("hello this is first state"); i < arr.length; i++) {
             System.out.println(arr[i]);
+            System.out.println("end");
         }
     }
 }
