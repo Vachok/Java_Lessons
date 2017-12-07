08535aa8204717a4de805af43ad62333e8721b6f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 12:41:58 +0300

    если бросаем из метода нужно бросать до самого "верхнего"
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main( String[] args ) {
-        try {
+    public static void main( String[] args ) throws Exception {
             new Main().meMethod();
-        } catch (IOException e) {
-        }
     }
 
     void meMethod() throws IOException {
@@ -14,8 +11,6 @@ public class Main {
             throw new IOException();
     }
 }
-
-
 
 
 
