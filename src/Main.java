5c330fb07a87cb3b5bd488b26770e22cc3c7c6a8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 09:08:51 +0300

    Пробегаем по массиву 1 2 3 https://youtu.be/Q2DXFrzYWJs?t=2m40s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        int i = 5;
-        while (i > 0) {
-            System.out.println(i);
-            i--;
+        int i = 0;
+        while (i < arr.length) { //пока true - выполняется
+            System.out.println(arr[i]);
+            i++;
         }
     }
 }
