1002b16b45dfef8b6e2809a044ece59cd1fa94e7
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:11:16 +0300

    переменных можно задать много
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        for (int i = 0; i < arr.length; ) {
+        for (double i = 0, j = 3, qwe = 123; i < arr.length; ) {
             System.out.println(arr[i]);
             i++;
         }
