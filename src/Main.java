fa1bf2f96ac3cc59d3d18ba12f1dab54fe0d801d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 14:57:13 +0300

    Process finished with exit code 0 abc abcdef
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main( String[] args ) {
         String s = new String("abc");
-        s.concat("def");
+        String s2 = s.concat("def");
         System.out.println(s);
+        System.out.println(s2);
     }
 }
