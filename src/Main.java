c32f501597c47f49645897ce61e66385692ecaeb
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 15:06:37 +0300

    -abcdef Можно вставить символ https://youtu.be/d_oou6CAz5U?t=6m9s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         StringBuilder stringBuilder = new StringBuilder("abc");
         stringBuilder.append("def");
+        stringBuilder.insert(0 , "-");
         System.out.println(stringBuilder);
     }
 }
