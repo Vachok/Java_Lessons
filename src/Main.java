3e88bf4418e012e4f25f333e7a991773569c5e12
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:05:38 +0300

    можно объединять логическими операторами https://youtu.be/6Vnm9T4NC2k?t=3m31s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        for (; 5 < 6; ) {
+        for (; 5 < 6 && arr[1] > arr[2] || true; ) { //boolean-выражение. можно объединять &&
             System.out.println(arr[1]);
         }
     }
