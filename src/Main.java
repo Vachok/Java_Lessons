e1460dd9f5db894ce058c30a4e90bdcae06ca324
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 14:38:22 +0300

    По-умолчанию assert - отключен
    -5
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 
 public class Main {
     public static void main( String[] args ) {
+        new Main().myMethod(-5);
     }
 
     private void myMethod( int a ) {
         assert (a > 0);
+        System.out.println(a);
     }
 }
