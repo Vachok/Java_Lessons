2049563d0878d6665842202936b8d3e6d3095c22
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 15:37:51 +0300

    Java_Lessons-60 45: Работа с файлами и директориями пусто! мы не знаем что за объект в памяти
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-
+import java.io.File;
 
 public class Main {
     public static void main( String[] args ) {
-
+        File file = new File("temp.txt");
+        if (file.isDirectory()) {
+            System.out.println("dir");
+        }
+        if (file.isFile()) {
+            System.out.println("file");
+        }
     }
 }
