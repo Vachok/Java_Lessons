edb9e389823f2390338ee2ee5510e3a144724bfa
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 21:55:14 +0300

    equals hashcode https://youtu.be/vz6nyRXCUs0?t=16m38s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/User.java
+++ src/User.java
@@ -4,4 +4,22 @@ public class User implements Serializable {
     int lifeLevel;
     static int staticField;
     transient Sword sword;
+
+    @Override
+    public boolean equals( Object o ) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (lifeLevel != user.lifeLevel) return false;
+        return sword.equals(user.sword);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lifeLevel;
+        result = 31 * result + sword.hashCode();
+        return result;
+    }
 }
