bb8ec043e429423ac65f24c9f4905d9ae2aa0d77
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 11:29:54 +0300

    тут ничего не бросаем try finally try1 https://youtu.be/mLpMtc62530?t=12m29s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         try {
             System.out.println("try");
             Object o = null;
-            o.hashCode();
+            // o.hashCode();
             return "try1";
         } catch (NullPointerException e) {
             System.out.println("catch");
@@ -19,7 +19,6 @@ public class Main {
         } finally {
             System.out.println("finally");
             //return "finally2";
-
         }
         }
     }
