51c9b5064f70b4a01699a2c7fa0ddeda8fcd788e
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:01:29 +0300

    параметры для for необязательны бесконечно будет выводить 2 https://youtu.be/6Vnm9T4NC2k?t=2m28s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        for (int j = 0; j < arr.length; j++) {
-            System.out.println(arr[j]);
+        for (; ; ) {
+            System.out.println(arr[1]);
         }
     }
 }
