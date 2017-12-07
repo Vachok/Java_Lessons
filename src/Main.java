242d67e356095157ef0a295f69186242c20408ef
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 09:15:13 +0300

    Java_Lessons-51 39: Циклы while и do while 1 https://youtu.be/Q2DXFrzYWJs?t=5m29s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        while (i < arr.length) { //пока true - выполняется
+        while (i == 5) {
             System.out.println(arr[i]);
             i++;
         }
@@ -12,7 +12,7 @@ public class Main {
         do {
             System.out.println(arr[i]);
             i++;
-        } while (i < arr.length);
+        } while (false);
 
     }
 }
