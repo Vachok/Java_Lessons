36a59bc0d035fc1d9a6e06a7af6ade719b49da4d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 14:44:48 +0300

    Механизм Assertions Используется при разработке, особенно совместной. При релизах, как правило, выключен.
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,3 @@ public class Main {
         System.out.println(a);
     }
 }
-
-//    Механизм Assertions - добавить -ea в VM Options
-//        Exception in thread "main" java.lang.AssertionError
-//        at Main.myMethod(Main.java:9)
-//        at Main.main(Main.java:5)
