c7630a5190dbea4a8901b90996458e43d68cd98d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 09:45:09 +0300

    Java_Lessons-52 40: Цикл For + 41: Улучшенный цикл For аналог while 1 2 3 1 2 3 https://youtu.be/6Vnm9T4NC2k?t=51s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        while (arr.length) {
+        while (i < arr.length) {
             System.out.println(arr[i]);
             i++;
+        }
+        for (int j = 0; j < arr.length; j++) { // аналог while
+            System.out.println(arr[j]);
         }
     }
 }
