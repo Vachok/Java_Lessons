906a40320ffc434128b792e804300fb9ae8bb39f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 09:05:12 +0300

    Java_Lessons-51 39: Циклы while и do while 5 4 3 2 1
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main( String[] args ) {
-
+        int[] arr = {1 , 2 , 3};
+        int i = 5;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
     }
 }
