9f95021e8cfcce0267fbfb9da834e7dfb2d6900c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:07:38 +0300

    (2)... потому что i не увеличивается
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        int i = 0;
-        for (; 5 < 6 && arr[1] > arr[2] || true; ) { //boolean-выражение. можно объединять &&
+        for (int i = 0; i < arr.length; ) {
             System.out.println(arr[1]);
         }
     }
