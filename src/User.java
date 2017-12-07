02d7d0cd195c532d1c47d03a64dd28fd714e847b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 22:00:53 +0300

    Сериализация: https://youtu.be/vz6nyRXCUs0?t=18m6s
    Объект, помечаем Serializable;
    Примитивы сериализуются. Объекты или transient или должны быть Serialized
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/User.java
+++ src/User.java
@@ -3,23 +3,5 @@ import java.io.Serializable;
 public class User implements Serializable {
     int lifeLevel;
     static int staticField;
-    transient Sword sword;
-
-    @Override
-    public boolean equals( Object o ) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (lifeLevel != user.lifeLevel) return false;
-        return sword.equals(user.sword);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = lifeLevel;
-        result = 31 * result + sword.hashCode();
-        return result;
-    }
+    transient Sword sword; //transient не должен учавствовать в хеш
 }
