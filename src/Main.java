de469a986e32c2b986bbff2732ddfdb66f9bf16f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 11:03:43 +0300

    try java.lang.NullPointerException catch 	at Main.someMethod(Main.java:12) finally 	at Main.main(Main.java:6)
    Process finished with exit code 0
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.io.IOException;
 
 public class Main {
+
     public static void main( String[] args ) {
         new Main().someMethod();
     }
-
     void someMethod() {
         try {
+            System.out.println("try");
             Object o = null;
             o.hashCode();
         } catch (NullPointerException e) {
+            System.out.println("catch");
             e.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
     }
 }
