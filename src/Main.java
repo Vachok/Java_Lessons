c94ae110e13c046daaa1fea87d3fa2cccc336f10
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:43:36 +0300

    Java_Lessons-53 42: Exceptions - исключения try - catch https://youtu.be/mLpMtc62530?t=3m24s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 
 public class Main {
     public static void main( String[] args ) {
+        new Main().someMethod();
+    }
 
+    void someMethod() {
+        try {
+            Object o = null;
+            o.hashCode();
+            // throw new Exception();
+        } catch (Exception e) {
+
+        }
     }
 }
