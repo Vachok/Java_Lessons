8ac94c773f2477e54eb196f98f3c273876b991fb
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 12:39:20 +0300

    throws https://youtu.be/mLpMtc62530?t=32m36s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main( String[] args ) {
-        new Main().meMethod();
+        try {
+            new Main().meMethod();
+        } catch (IOException e) {
+        }
     }
 
     void meMethod() throws IOException {
