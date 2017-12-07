80c029c4b2f17c1a0a4cd664fe93653fe12c4b89
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:08:23 +0300

    2 2 2 https://youtu.be/6Vnm9T4NC2k?t=4m16s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        for (int i = 0; i < arr.length; ) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[1]);
         }
     }
