164f5395cc35ec8aa58941ada934e3956d60d27e
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 09:40:25 +0300

    Java_Lessons-52 40: Цикл For + 41: Улучшенный цикл For
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main( String[] args ) {
-
+        int[] arr = {1 , 2 , 3};
+        int i = 0;
+        while (arr.length) {
+            System.out.println(arr[i]);
+            i++;
+        }
     }
 }
