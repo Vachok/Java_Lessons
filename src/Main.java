f817df55f24c43bdd9422a4976c37530c06d074b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:21:32 +0300

    может быть без скобок hello this is first state 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
         int i = 0;
-        for (System.out.println("hello this is first state"); i < arr.length; i++) {
+        for (System.out.println("hello this is first state"); i < arr.length; i++)
             System.out.println(arr[i]);
-            System.out.println("end");
-        }
     }
 }
