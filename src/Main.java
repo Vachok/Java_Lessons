8bfdaba042c3a5bf1a57fd3e8c14fc6732315937
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 12:48:27 +0300

    Runtime - ошибка в коде. массива args - нет. Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 5 	at Main.main(Main.java:6)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main( String[] args ) {
+        System.out.println(args[5]);
             new Main().meMethod();
     }
 
@@ -21,7 +22,7 @@ public class Main {
 1. Throwable
     2. Error - не нужно ловить! Это риторические проблемы - сами придут.
     2. Exception -
-        3. RuntimeException
+        3. RuntimeException - это "ошибки программирования"
         3. IO
         3 ....можно добавить свои подклассы
 
