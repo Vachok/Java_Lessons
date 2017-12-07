6ba166846dbdee7fa867d761cf049adf0ac1abaa
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 10:11:58 +0300

    должны быть однотипные. так нельзя
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main( String[] args ) {
         int[] arr = {1 , 2 , 3};
-        for (double i = 0, j = 3, qwe = 123; i < arr.length; ) {
+        for (int i = 0, j = 3, double qwe = 123;
+        i<arr.length ){
             System.out.println(arr[i]);
             i++;
         }
