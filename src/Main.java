de324aa522d6fded35042d1d09ef7d08b8588624
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 12:07:31 +0300

    java.lang.RuntimeException - finally выполнен try 	at Main.someMethod(Main.java:12) 	at Main.main(Main.java:6) finally
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,16 @@ public class Main {
 
     public static void main( String[] args ) {
         new Main().someMethod();
-        System.out.println("after method");
     }
 
     void someMethod() {
         try {
             System.out.println("try");
-            while (true) {
-            }
+            throw new RuntimeException();
             //Object o = null;
             // o.hashCode();
         } finally {
             System.out.println("finally");
-            throw new RuntimeException();
         }
         }
     }
