f226378058cf4e72cd501e6614d511a4d41fcd21
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-07 12:33:03 +0300

    Java_Lessons-53 42: Exceptions - исключения
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,22 @@ public class Main {
     void meMethod() {
         try {
             System.out.println("try");
-            throw new Error();
-        } catch (Error e) {
+            throw new MyException();
+        } catch (MyException e) {
             System.out.println("catch");
         }
+    }
+
+    private class MyException extends Throwable {
     }
 }
 
 /* Иерархия Exceptions
 1. Throwable
     2. Error - не нужно ловить! Это риторические проблемы - сами придут.
-    2. Exception - можно добавить свои подклассы
+    2. Exception -
         3. RuntimeException
+        3. IO
+        3 ....можно добавить свои подклассы
+
  */
