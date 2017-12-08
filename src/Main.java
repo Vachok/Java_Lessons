53fcdfd0272b7ac54aff984f6dbf5dddded58598
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 13:16:00 +0300

    1 6 2 5
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         collection.add("2");
         collection.add("3");
         collection.set(2 , "5");
-        collection.add(6 , "6");
+        collection.add(1 , "6");
         for (int i = 0; i < collection.size(); i++) {
             System.out.println(collection.get(i));
         }
