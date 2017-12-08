2b5defc5b285a7bbbdb2e5e2a26c15a9c342a05f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:30:31 +0300

    Kernegi https://youtu.be/0sWpjUvbGcQ?t=25m20s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Map collection = new HashSet();
-        collection.put(new Object() , "Dan Brown");
+        Map collection = new HashMap();
+        collection.put("1" , "Dan Brown");
         collection.put("2" , "Kernegi");
         collection.put("3" , "Jack London");
-        for (Object o : collection) {
-            System.out.println(o);
-        }
+        System.out.println(collection.get("2"));
     }
 }
