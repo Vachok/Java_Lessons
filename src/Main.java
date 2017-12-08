16c4136fb51364c217b8f9ede4dbe8ebb1755cbc
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 12:46:06 +0300

    https://youtu.be/0sWpjUvbGcQ?t=3m45s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main( String[] args ) {
-        int[] array = new int[10];
+        int[] array = new int[15];
+        Collection collection = new ArrayList();
+        Collections collections;
 
     }
 }
