9e8c537dfe75472c83404a4f7e6b8d343a0824d6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 17:12:36 +0300

    Java_Lessons-75 Java 51: Сортировка коллекций и интерфейсы - Comparable https://youtu.be/x4CUbW-K8E8?t=2m17s Exception in thread "main" java.lang.ClassCastException: Person cannot be cast to java.base/java.lang.Comparable 	at java.base/java.util.TreeMap.compare(TreeMap.java:1291) 	at java.base/java.util.TreeMap.put(TreeMap.java:536) 	at java.base/java.util.TreeSet.add(TreeSet.java:255) 	at Main.main(Main.java:14)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import java.util.Set;
 import java.util.TreeSet;
 
+class Person {
+    int age;
+
+    public Person( int age ) {
+        this.age = age;
+    }
+}
+
 public class Main {
     public static void main( String[] args ) {
         Set set = new TreeSet<>();
-        set.add("2");
-        set.add("5");
-        set.add("4");
-        set.add("1");
+        set.add(new Person(4));
+        set.add(new Person(6));
+        set.add(new Person(5));
+        set.add(new Person(2));
         for (Object o : set) {
             System.out.println(o);
         }
