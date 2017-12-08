2a2ffae125ed866ef114b51e5cb49d71c69e6a07
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:29:04 +0300

    Если бы тут стоял Object (ключом), ему нужно было бы переопределить hashMap
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Set collection = new HashSet();
-        collection.add("1");
-        collection.add("2");
-        collection.add("3");
-        collection.add("3");
-        collection.add("3");
-        collection.add("3");
+        Map collection = new HashSet();
+        collection.put(new Object() , "Dan Brown");
+        collection.put("2" , "Kernegi");
+        collection.put("3" , "Jack London");
         for (Object o : collection) {
             System.out.println(o);
         }
