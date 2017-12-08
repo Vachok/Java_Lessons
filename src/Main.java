3a3af7465d76370911a851c4de0498b0c6717106
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 12:49:28 +0300

    1 3 https://youtu.be/0sWpjUvbGcQ?t=6m43s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
         int[] array = new int[15];
         Collection collection = new ArrayList();
-        Collections collections;
-
+        collection.add("1");
+        collection.add("2");
+        collection.add("3");
+        collection.remove("2");
+        Iterator iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
