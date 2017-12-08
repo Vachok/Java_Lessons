8e277dbdc566adfb3a0a0f3d3cfdc63a414306d9
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 10:55:44 +0300

    toString - необязательно
    Main@6a5fc7f7
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main( String[] args ) {
         Main main = new Main();
-        System.out.println(main.toString());
+        System.out.println(main + "");
     }
 }
