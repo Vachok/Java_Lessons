0b4943333d6496e7560770c1726ce72dd7fd882a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 09:51:44 +0300

    Более удобный класс, для работы с датами. Thu Jan 01 03:00:00 MSK 1970 java.util.GregorianCalendar[time=0,areFieldsSet=true,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id="Europe/Moscow",offset=10800000,dstSavings=0,useDaylight=false,transitions=79,lastRule=null],firstDayOfWeek=2,minimalDaysInFirstWeek=1,ERA=1,YEAR=1970,MONTH=0,WEEK_OF_YEAR=1,WEEK_OF_MONTH=1,DAY_OF_MONTH=1,DAY_OF_YEAR=1,DAY_OF_WEEK=5,DAY_OF_WEEK_IN_MONTH=1,AM_PM=0,HOUR=3,HOUR_OF_DAY=3,MINUTE=0,SECOND=0,MILLISECOND=0,ZONE_OFFSET=10800000,DST_OFFSET=0]
    https://youtu.be/EwefevRSHeI?t=3m49s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     public static void main( String[] args ) {
         Date date = new Date(0);
         System.out.println(date);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        System.out.println(calendar);
     }
 }
