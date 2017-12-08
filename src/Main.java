92bcd46484657534f59360aa5c57ae1b073f56bf
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:37:16 +0300

    Пробегаем по-ключам коллекции https://youtu.be/0sWpjUvbGcQ?t=28m 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         collection.put("2" , "Kernegi");
         collection.put("3" , "Jack London");
         System.out.println(collection.get("2"));
-        Set set = collection.entrySet();
+        Set set = collection.keySet();
         for (Object o : set) {
             System.out.println(o);
         }
