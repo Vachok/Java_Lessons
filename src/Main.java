f281a2d629c75bae7a65f3b933d461199a1d7d27
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 13:21:06 +0300

    1 1 1 3 https://youtu.be/0sWpjUvbGcQ?t=16m36s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Queue collection = new ArrayList();
-        collection.add("1");
-        collection.add("2");
-        collection.add("3");
-        collection.add(1 , "6");
+        Queue collection = new PriorityQueue();
+        collection.offer("1");
+        collection.offer("2");
+        collection.offer("3");
         for (int i = 0; i < collection.size(); i++) {
+            System.out.println(collection.peek());
         }
+        System.out.println(collection.size());
     }
 }
