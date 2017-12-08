9dcdc593817657c72b09a6cbc988ee34be20e8dc
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 13:17:49 +0300

    Queue https://youtu.be/0sWpjUvbGcQ?t=14m27s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        List collection = new ArrayList();
+        Queue collection = new ArrayList();
         collection.add("1");
         collection.add("2");
         collection.add("3");
-        collection.set(2 , "5");
         collection.add(1 , "6");
         for (int i = 0; i < collection.size(); i++) {
-            System.out.println(collection.get(i));
         }
     }
 }
