9cdd1e0ea37831ab24490bb08cac90537d3078e7
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:21:18 +0300

    Set. Коллекция уникальных объектов https://youtu.be/0sWpjUvbGcQ?t=20m27s 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Queue collection = new PriorityQueue();
-        collection.offer("1");
-        collection.offer("2");
-        collection.offer("3");
-        Iterator iterator = collection.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(collection.poll());
+        Set collection = new HashSet();
+        collection.add("1");
+        collection.add("2");
+        collection.add("3");
+        collection.add("3");
+        collection.add("3");
+        collection.add("3");
+        for (Object o : collection) {
+            System.out.println(o);
         }
-        System.out.println("num of elements: " + collection.size());
     }
 }
