722ece691fde3526422cf5b007c20d3fd4ecb58d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:58:19 +0300

    Нет гарантиии, что элементы будут выведены по-порядку https://youtu.be/0sWpjUvbGcQ?t=32m0s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        // Элементы, для замены массивов
-        // https://youtu.be/0sWpjUvbGcQ?t=29m0s
-        Collections collections; // основной класс
-        List list; // отличается индексацией. данные хранятся и выводятся по-индексам
-        Set set; // выводит уникальные значения
-        Queue queue; // очередь. как передали - так получили
-        Map collection = new HashMap(); // объекты хранимые по-ключу
-        collection.put("1" , "Dan Brown");
-        collection.put("2" , "Kernegi");
-        collection.put("3" , "Jack London");
-        System.out.println(collection.get("2"));
-        Set set = collection.keySet();
-        for (Object o : set) {
+        Collection collection = new HashSet();
+        collection.add("1");
+        collection.add("2");
+        collection.add("3");
+        for (Object o : collection) {
             System.out.println(o);
         }
     }
 }
-
