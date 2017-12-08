525e81fe744686b592fa4cc44f86228dd8ffb0f8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 17:09:39 +0300

    Lessons-75 Сортировка коллекций и интерфейсы - Comparable и Comparator https://youtu.be/x4CUbW-K8E8?t=1m22s 1 2 4 5
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main( String[] args ) {
-
+        Set set = new TreeSet<>();
+        set.add("2");
+        set.add("5");
+        set.add("4");
+        set.add("1");
+        for (Object o : set) {
+            System.out.println(o);
+        }
     }
 }
