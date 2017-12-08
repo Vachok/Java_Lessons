27b6f5d50a8aed049e50f97213d147d56d89fb80
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 09:48:25 +0300

    Thu Jan 01 03:00:00 MSK 1970 https://youtu.be/EwefevRSHeI?t=1m43s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main( String[] args ) {
-        Date date = new Date();
+        Date date = new Date(0);
         System.out.println(date);
     }
 }
