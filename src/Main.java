518f9483e95536e686a0a6dabc26883350bf9ff7
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 12:57:07 +0300

    2 https://youtu.be/0sWpjUvbGcQ?t=11m0s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,9 @@ import java.util.*;
 public class Main {
     public static void main( String[] args ) {
         List collection = new ArrayList();
-        Set collection = new HashSet();
-        Queue collection = new PriorityQueue();
-        Map map = new HashMap();
         collection.add("1");
         collection.add("2");
         collection.add("3");
-        collection.remove("2");
-        for (Object o : collection) {
-            System.out.println(o);
-        }
+        System.out.println(collection.get(1));
     }
 }
