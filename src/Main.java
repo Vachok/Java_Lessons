73a5736cd98eba3561376234ee25f32a7a54f3d5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 11:00:07 +0300

    Можно переопределить методы. Напр.
    https://youtu.be/DrQqZj5OIZc?t=3m56s
    5
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 
 
 public class Main {
+    int value;
     public static void main( String[] args ) {
         Main main = new Main();
+        main.value = 5;
         System.out.println(main + "");
+    }
+
+    @Override
+    public String toString() {
+        return value + "";
     }
 }
