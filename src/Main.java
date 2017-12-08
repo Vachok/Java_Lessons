c6de5b23e24358b452dd469d53c21d68f30fab88
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 12:54:23 +0300

    https://youtu.be/0sWpjUvbGcQ?t=9m50s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        int[] array = new int[15]; // быстро, но нужно знать размер
-        Collection collection = new ArrayList();
+        List collection = new ArrayList();
+        Set collection = new HashSet();
+        Queue collection = new PriorityQueue();
+        Map map = new HashMap();
         collection.add("1");
         collection.add("2");
         collection.add("3");
         collection.remove("2");
-        Iterator iterator = collection.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
         for (Object o : collection) {
             System.out.println(o);
         }
