d8602e67a677acf2464884c4cb5ca6f0384116e3
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 15:54:33 +0300

    Коллекции. Основные элементы
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.util.*;
 
-//Map - коллекция с ключами
 public class Main {
     public static void main( String[] args ) {
-        Map collection = new HashMap();
+        // Элементы, для замены массивов
+        // https://youtu.be/0sWpjUvbGcQ?t=29m0s
+        Collections collections; // основной класс
+        List list; // отличается индексацией. данные хранятся и выводятся по-индексам
+        Set set; // выводит уникальные значения
+        Queue queue; // очередь. как передали - так получили
+        Map collection = new HashMap(); // объекты хранимые по-ключу
         collection.put("1" , "Dan Brown");
         collection.put("2" , "Kernegi");
         collection.put("3" , "Jack London");
@@ -14,3 +19,4 @@ public class Main {
         }
     }
 }
+
