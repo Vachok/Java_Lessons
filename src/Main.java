0e911359d738db8768c120b1aafc15d33b27f327
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 12:51:24 +0300

    1 3 1 3 https://youtu.be/0sWpjUvbGcQ?t=7m33s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        int[] array = new int[15];
+        int[] array = new int[15]; // быстро, но нужно знать размер
         Collection collection = new ArrayList();
         collection.add("1");
         collection.add("2");
@@ -11,6 +11,9 @@ public class Main {
         Iterator iterator = collection.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
+        }
+        for (Object o : collection) {
+            System.out.println(o);
         }
     }
 }
