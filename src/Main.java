ad319e7925269921f25de58022dbfb764bac225f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 16:02:17 +0300

    Отсортированные коллекции https://youtu.be/0sWpjUvbGcQ?t=34m56s 1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Collection collection = new LinkedHashSet();
-        collection.add("3");
+        Collection collection = new TreeSet();
         collection.add("2");
+        collection.add("3");
         collection.add("1");
         for (Object o : collection) {
             System.out.println(o);
