5e155378aed353dc4b1b63e7225d1af66ab9b780
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 09:52:44 +0300

    Текущая дата: Fri Dec 08 09:52:22 MSK 2017 java.util.GregorianCalendar[time=1512715942959,areFieldsSet=true,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id="Europe/Moscow",offset=10800000,dstSavings=0,useDaylight=false,transitions=79,lastRule=null],firstDayOfWeek=2,minimalDaysInFirstWeek=1,ERA=1,YEAR=2017,MONTH=11,WEEK_OF_YEAR=50,WEEK_OF_MONTH=2,DAY_OF_MONTH=8,DAY_OF_YEAR=342,DAY_OF_WEEK=6,DAY_OF_WEEK_IN_MONTH=2,AM_PM=0,HOUR=9,HOUR_OF_DAY=9,MINUTE=52,SECOND=22,MILLISECOND=959,ZONE_OFFSET=10800000,DST_OFFSET=0]
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class Main {
     public static void main( String[] args ) {
-        Date date = new Date(0);
+        Date date = new Date();
         System.out.println(date);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
