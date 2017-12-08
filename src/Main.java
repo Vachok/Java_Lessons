5137b3604e7fbfb296878c3d1542735484199e2c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-08 16:00:13 +0300

    1 2 3
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Collection collection = new LinkedHashSet();
-        collection.add("1");
-        collection.add("2");
+        Collection collection = new HashSet();
         collection.add("3");
+        collection.add("2");
+        collection.add("1");
         for (Object o : collection) {
             System.out.println(o);
         }
