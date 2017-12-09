6b4094dc05e1983043c1a7a6dc76b882c42c1147
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 10:05:46 +0300

    Java_Lessons-76 Java 52: Списки - List
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main( String[] args ) {
-
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
