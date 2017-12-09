329fe5151bd202659c7cbdb4bb4c4e2af69ddca2
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 12:18:31 +0300

    PriorityQueue() https://youtu.be/gLn9J_SMd84?t=5m37s // похож на LinkedList, но сортирует
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.util.Queue;
 
 public class Main {
     public static void main( String[] args ) {
-        Queue queue = new LinkedList(); //двунапр. список
-        Queue queue2 = new PriorityQueue();
+        Queue queue = new LinkedList(); // двунапр. список. очередь в том порядке, как добавили
+        Queue queue2 = new PriorityQueue(); // похож на LinkedList, но сортирует
         queue.offer("1");
         queue.offer("3");
         queue.offer("2");
