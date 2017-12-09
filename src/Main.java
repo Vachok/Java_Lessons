8776e6becf67f10370f4da7ac4b91952009e3849
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 10:44:28 +0300

    List https://youtu.be/e9ImyEq5a1g?t=24m2s Списки на основе индексов Хранить можно всё Array, Linked, Vector
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main( String[] args ) {
         List list = new ArrayList(); // коллек. на основании индекса
         System.out.print(list.size() + " /// ");
-        List list1 = new Vector();
-        List linked = new LinkedList();
+        List list1 = new Vector(); // ArrayList synchronized
+        List linked = new LinkedList(); // implements Queue. Двунаправленный список
         list.add(1);
         list.add(2);
         list.add(3);
