66d2d0334736d6275b1aa66e74aee87f09a2d15c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 09:26:07 +0300

    создание отсортированной коллекции https://youtu.be/x4CUbW-K8E8?t=7m7s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -20,12 +20,12 @@ class Person implements Comparable<Person> {
 }
 
 public class Main {
-    public static void main( String[] args ) {
-        Set set = new TreeSet<>();
-        set.add(new Person(4));
-        set.add(new Person(6));
-        set.add(new Person(4));
-        set.add(new Person(2));
+    public static void main( String[] args ) { //https://youtu.be/x4CUbW-K8E8?t=7m7s
+        Set set = new TreeSet<>(); //создание отсортированной коллекции
+        set.add(new Person(4)); // элемент коллекции
+        set.add(new Person(6)); // элемент коллекции
+        set.add(new Person(5)); // элемент коллекции
+        set.add(new Person(2)); // элемент коллекции
         for (Object o : set) {
             System.out.println(o);
         }
