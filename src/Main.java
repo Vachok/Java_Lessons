642f2fc5354aec647018ab288671c228c2899960
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 22:16:22 +0300

    Map https://youtu.be/t4ujPYrkn6Y?t=31m43s Интерфейс, позволяющий доставать и ложить объекты с помощью ключей _HashMap_ - хранит объекты в виде массива нодов не дает гарантии отдачи в определённой последовательности. _LinkedHashMap_ - запоминает первый и последний элементы, хранит порядок. Отдаёт как получил. _TreeMap_ - отсортирован. Есть коренной элемент. Алгоритм Red-Black.
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+/*public class Main {
     public static void main( String[] args ) {
         Map map3 = new HashMap();
         Map map2 = new Hashtable();
