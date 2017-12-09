3e11a40ef6b2d4cb5609aa26f6a1baa2bf76b11d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 22:04:27 +0300

    TreeMap https://youtu.be/t4ujPYrkn6Y?t=26m0s Отсортированный список
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main( String[] args ) {
         Map map3 = new HashMap();
         Map map2 = new Hashtable();
-        Map map = new LinkedHashMap(); // упорядоченный HM
-        Map map4 = new TreeMap();
+        Map map = new LinkedHashMap();
+        Map map4 = new TreeMap(); //отсортированный список
         map.put("1" , "one");
         map.put("3" , "three");
         map.put("2" , "two");
         System.out.println(map.get("3"));
         Set set = map.entrySet();
         for (Object o : set) {
-            System.out.print(o);
+            System.out.print(o + " ");
         }
     }
 }
