78b17791900d1db1a8aeabb4c6204998d69f087d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 17:55:25 +0300

    HashMap хранит так: https://youtu.be/t4ujPYrkn6Y?t=19m26s Массив нодов, кладёт в одну ячейку. Нужно переопределить eq_hC
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main( String[] args ) {
-        Map map = new HashMap(); // хранит объекты, на основании Хэша. если ключ повторяется - кладёт в одну "ячейку"
-        /*  Obj.hash = 3
-            Obj.hash = 3     Obj.hash =1
-        ___ ______________ ____________ (RAM)
-        * */
+        Map map = new HashMap();
         Map map2 = new Hashtable();
         Map map3 = new LinkedHashMap();
         Map map4 = new TreeMap();
-        map.put("1" , "one"); // в массив table, где хранятся ноды
-        map.put("3" , "three"); // создается перем. tab, добавляется в table, после вычисл. хэш.
-        map.put("2" , "two"); // проверка - есть ли объект с таким хэш. если нет - создает новый.
+        map.put("1" , "one");
+        map.put("3" , "three");
+        map.put("2" , "two");
         System.out.println(map.get("3"));
         Set set = map.entrySet();
         for (Object o : set) {
