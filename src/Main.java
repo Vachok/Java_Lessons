fe9c7ec2b4b67a095c1f236db309b12715a22b13
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 17:28:02 +0300

    Вытаскиваем элемент с ключом 3 https://youtu.be/t4ujPYrkn6Y?t=1m45s three Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Map;
 
 public class Main {
     public static void main( String[] args ) {
-        Map map = new HashMap();
+        Map map = new HashMap(); // тут если _**не**_ String - нужно было бы переопределить equals и hashCode
         map.put("1" , "one");
         map.put("3" , "three");
         map.put("2" , "two");
+        System.out.print(map.get("3"));
     }
 }
