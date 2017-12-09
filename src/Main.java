270d666d22b5559c6cc56be1bf695cc3bf286d42
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 12:54:33 +0300

    Maps https://youtu.be/t4ujPYrkn6Y?t=1m5s Коллкуция, хранящая элементы по-ключу
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main( String[] args ) {
-
+        Map map = new HashMap();
+        map.put("1" , "one");
+        map.put("3" , "three");
+        map.put("2" , "two");
     }
 }
