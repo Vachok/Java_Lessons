7fea7ea1cee6b06f3b3bb61dfe22f60a9296a417
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 10:57:31 +0300

    2 типа очереди https://youtu.be/gLn9J_SMd84?t=55s Priority, LinkedList
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
     public static void main( String[] args ) {
-
+        Queue queue = new LinkedList();
+        Queue queue2 = new PriorityQueue();
     }
 }
