92903cfbbae1915532129957ab259e7ca945e6c9
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-09 09:41:13 +0300

    private int - используем Getter https://youtu.be/x4CUbW-K8E8?t=12m21s
    Сравниваются классы
    Есть сортированная коллекция (TreeSet)
    примитивы имеют механизм сравнения
    объекты - переопределить сравниваемое или
    Getter|Setter
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ import java.util.Set;
 import java.util.TreeSet;
 
 class Person {
-    int age;
+    private int age;
 
     public Person( int age ) {
         this.age = age;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     @Override
@@ -18,14 +22,13 @@ class Person {
 class ComparePerson implements Comparator<Person> {
     @Override
     public int compare( Person o1 , Person o2 ) {
-        return o1.age - o2.age;
+        return o1.getAge() - o2.getAge();
     }
 }
 
 public class Main {
     public static void main( String[] args ) {
-        ComparePerson comparePerson = new ComparePerson();
-        Set set = new TreeSet<>(comparePerson);
+        Set set = new TreeSet(new ComparePerson());
         set.add(new Person(4));
         set.add(new Person(6));
         set.add(new Person(5));
