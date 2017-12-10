a812ccc449417e0dd92eb331edc1b81a979298c1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 09:40:02 +0300

    Sets https://youtu.be/NsKo2zFwdFo?t=1m Коллекция, хранящая уникальные объекты 2 1 Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main( String[] args ) {
-
+        Set set = new HashSet();
+        set.add("1 ");
+        set.add("2 ");
+        set.add("2 ");
+        set.add("1 ");
+        set.add("1 ");
+        for (Object o : set) {
+            System.out.print(o);
+        }
     }
 }
