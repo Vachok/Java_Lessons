8cc09ceb6e1b1b80b1d36b8ac08948366634395b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 09:43:23 +0300

    HashSet https://youtu.be/NsKo2zFwdFo?t=3m37s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main( String[] args ) {
-        Set set = new HashSet();
+        Set set = new HashSet(); // неупорядоченные
+        Set set2 = new LinkedHashSet(); // в порядке добавления
+        Set set3 = new TreeSet(); // сортировка
         set.add("1 ");
         set.add("2 ");
         set.add("2 ");
