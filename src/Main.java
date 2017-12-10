eb5412d1d5aa7785cd33a5eedd6f98b6e3d53c60
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 17:51:03 +0300

    BROKEN! https://youtu.be/LF8ETKQQEXg?t=17m07s Information:java: Some messages have been simplified; recompile with -Xdiags:verbose to get full output Information:java: Errors occurred while compiling module 'Java_Lessons' Information:javac 9.0.1 was used to compile java sources Information:10.12.2017 17:50 - Compilation completed with 3 errors and 0 warnings in 1s 511ms C:\My_Proj\Java_Lessons\src\Main.java Error:(18, 27) java: incompatible types: java.util.ArrayList<java.lang.String> cannot be converted to java.util.List<Main> Error:(20, 13) java: no suitable method found for add(java.lang.String)     method java.util.Collection.add(Main) is not applicable       (argument mismatch; java.lang.String cannot be converted to Main)     method java.util.List.add(Main) is not applicable       (argument mismatch; java.lang.String cannot be converted to Main) Error:(23, 19) java: no suitable constructor found for File(no arguments)     constructor java.io.File.File(java.lang.String,int) is not applicable       (actual and formal argument lists differ in length)     constructor java.io.File.File(java.lang.String,java.io.File) is not applicable       (actual and formal argument lists differ in length)     constructor java.io.File.File(java.lang.String) is not applicable       (actual and formal argument lists differ in length)     constructor java.io.File.File(java.lang.String,java.lang.String) is not applicable       (actual and formal argument lists differ in length)     constructor java.io.File.File(java.io.File,java.lang.String) is not applicable       (actual and formal argument lists differ in length)     constructor java.io.File.File(java.net.URI) is not applicable       (actual and formal argument lists differ in length) Information:java: C:\My_Proj\Java_Lessons\src\Main.java uses unchecked or unsafe operations. Information:java: Recompile with -Xlint:unchecked for details.
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 class Car extends Main implements Comparable, Serializable {
     @Override
@@ -8,24 +11,18 @@ class Car extends Main implements Comparable, Serializable {
     }
 }
 
-public class Main<T extends Main & Comparable & Serializable> {
-
-    T war;
+public class Main<T extends Main & Comparable & Serializable> { // удобны для хранения данных
 
     public static void main( String[] args ) {
 
-        Main<Car> main = new Main<>();
-        main.war = new Car();
-        main.method(new Car());
-    }
-
-    T method( T type ) {
-        type.meMethods();
-        return type;
-    }
-
-    void meMethods() {
-
-        System.out.println("bla");
+        List<Main> list = new ArrayList<String>(); // только Main
+        List list2 = new ArrayList(); // что угодно! плохо!
+        list.add("STring");
+        list2.add("");
+        list2.add(new Object());
+        list2.add(new File());
+        for (Object o : list2) {
+            File f = (File) o;
+        }
     }
 }
