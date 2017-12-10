7fe1426b1a0cbe06ac0c71d2827acbe1da0b9d24
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 13:58:21 +0300

    Generics - обобщения https://youtu.be/LF8ETKQQEXg?t=1m42s Метод, который заранее не знает с чем работать 123 Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 
 public class Main {
     public static void main( String[] args ) {
+        Main main = new Main();
+        main.method("123");
+    }
 
+    <T> T method( T type ) {
+        System.out.print(type);
+        return type;
     }
 }
