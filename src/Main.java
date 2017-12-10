620ef51134e359445baed6c9b2bb40795a2c0a42
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 14:03:32 +0300

    Generics - обобщения https://youtu.be/LF8ETKQQEXg?t=3m49s Можно передавать множество типов.
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@
 public class Main {
     public static void main( String[] args ) {
         Main main = new Main();
-        main.method(new Main());
+        main.method("123");
     }
 
-    <T> T method( T type ) {
-        System.out.print(type);
-        return type;
+    <T, U, K> void method( T type , U u , K k ) {
+        System.out.print(s);
+        //return type;
     }
 }
