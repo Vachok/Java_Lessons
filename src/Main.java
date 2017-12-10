0f25605de031106a6831c10bff484a83f40b47dd
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-10 14:01:08 +0300

    Generics - обобщения https://youtu.be/LF8ETKQQEXg?t=2m30s Принимает всё Main@2d6a9952 Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main( String[] args ) {
         Main main = new Main();
-        main.method(1);
+        main.method(new Main());
     }
 
     <T> T method( T type ) {
