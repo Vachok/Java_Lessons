2c95f9b6c62eeeba2110e1c315a2d2e0132eaf39
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 17:18:04 +0300

    Process finished with exit code 0 https://youtu.be/mH1EJuyHvrQ?t=1m17s 2
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 
 public class Main {
     public static void main( String[] args ) {
-
+        MyObject myObject = new MyObject();
+        myObject.i = 1;
+        MyObject myObject1 = myObject;
+        myObject.i = 2;
+        System.out.println(myObject.i);
     }
+}
+
+
+
+class MyObject {
+    int i;
 }
