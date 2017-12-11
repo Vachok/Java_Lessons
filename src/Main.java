743d0350e14005e50f8492ea7d316b1660d7a71f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 09:29:11 +0300

    Java_Lessons-83 56: Generics - обобщения https://youtu.be/LF8ETKQQEXg?t=22m57s Generic - работает только на уровне компиляции
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main<T extends Main & Comparable & Serializable> {
 
     public static void main( String[] args ) {
 
-        List<File> list = new ArrayList<String>();
+        List<File> list = new ArrayList<>();
         Main main = new Main();
         main.method(list);
 
@@ -17,7 +17,14 @@ public class Main<T extends Main & Comparable & Serializable> {
     }
 
     void method( List list ) {
-
         list.add(new Main());
+    }
+}
+
+class MyClass<T> {
+    T t; //LOCALVARIABLE t Ljava/lang/Object; L0 L1 1
+
+    T method( T t ) {
+        return t;
     }
 }
