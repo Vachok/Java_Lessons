88a121ecd1350b8d148a92f0629310c3c3c6f3d5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 09:47:29 +0300

    Process finished with exit code 0 https://youtu.be/LF8ETKQQEXg?t=29m49s Child@64c64813
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,15 @@ public class Main {
 
     public static void main( String[] args ) {
 
-        List<Parrent> list = new ArrayList<Parrent>();
-        list.add(new Parrent());
+        List<Child> list = new ArrayList<>();
         list.add(new Child());
         Main main = new Main();
         main.method(list);
+    }
 
+    void method( List<? extends Parrent> list ) {
         for (Parrent parrent : list) {
             System.out.println(parrent);
         }
-    }
-
-    void method( List<Parrent> list ) {
     }
 }
