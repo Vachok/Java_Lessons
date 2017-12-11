623f5426ed98e72447f6a03fe1e4a158b5ac22f1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 18:37:28 +0300

    Java_Lessons-88 Java 59: Клонирование объектов https://youtu.be/mH1EJuyHvrQ?t=6m30s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main( String[] args ) throws Exception {
         MyObject myObject = new MyObject();
         myObject.i = 1;
+        NewObject newObject = new NewObject();
+        newObject.j = 3;
+        myObject.newObject = newObject;
         MyObject myObject1 = myObject.clone();
         myObject1.i = 2;
         System.out.println(myObject.i);
+        myObject1.newObject.j = 4;
+        System.out.print(myObject.newObject.j);
     }
 }
 
@@ -14,8 +19,17 @@ public class Main {
 
 class MyObject implements Cloneable {
     int i;
+
+    NewObject newObject;
     @Override
     protected MyObject clone() throws CloneNotSupportedException {
         return (MyObject) super.clone();
     }
+}
+
+
+
+class NewObject {
+
+    int j;
 }
