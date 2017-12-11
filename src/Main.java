ff242762ad943df20bbe03c208c146094140e1ec
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 14:14:59 +0300

    Java_Lessons-86 57: Inner Classes - Внутренние классы https://youtu.be/zxcXDVotj_4?t=1m31s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-
+class Popcorn {
+    void doSome() {
+        System.out.println("popcorn");
+    }
+}
 
 public class Main {
     public static void main( String[] args ) {
-
+        Popcorn popcorn = new Popcorn();
+        {
+            void doSome () {
+            System.out.println("main");
+        }
+        }
+        popcorn.doSome();
     }
 }
