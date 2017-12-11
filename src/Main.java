475571acdbe27c75d5113e063d19f6f4d1ebce5f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 17:24:37 +0300

    Process finished with exit code 1 https://youtu.be/mH1EJuyHvrQ?t=3m55s Exception in thread "main" java.lang.CloneNotSupportedException: MyObject 	at java.base/java.lang.Object.clone(Native Method) 	at MyObject.clone(Main.java:19) 	at Main.main(Main.java:7)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 
 public class Main {
-    public static void main( String[] args ) {
-        MyObject myObject = new MyObject(); // СОЗДАЁТСЯ объект (1 шт)
+    public static void main( String[] args ) throws Exception {
+        MyObject myObject = new MyObject();
         myObject.i = 1;
-        MyObject myObject1 = myObject; // получились 2 ссылки на 1 объект
-        myObject.i = 2;
+        MyObject myObject1 = myObject.clone();
+        myObject1.i = 2;
         System.out.println(myObject.i);
     }
 }
