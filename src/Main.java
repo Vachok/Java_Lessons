2e9fcfb8fbd85c59b915e52c0a99ce6c445ab88a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 17:32:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,5 @@
 
 public class Main {
     public static void main( String[] args ) throws Exception {
-        MyObject myObject = new MyObject();
-        myObject.i = 1;
-        MyObject myObject1 = myObject.clone();
-        myObject1.i = 2;
-        System.out.println(myObject.i);
-    }
-}
-
-
-
-class MyObject implements Cloneable {
-    int i;
-    @Override
-    protected MyObject clone() throws CloneNotSupportedException {
-        return (MyObject) super.clone();
     }
 }
