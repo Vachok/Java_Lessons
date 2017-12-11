eff4f20023fb6c8f3a0301f18901aa56f495f904
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-11 14:20:52 +0300

    Process finished with exit code 0 https://youtu.be/zxcXDVotj_4?t=2m47s main second
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@ class Popcorn {
     void doSome() {
         System.out.println("popcorn");
     }
+    void secondMethhod() {
+        System.out.println("second");
+    }
 }
 
 public class Main {
     public static void main( String[] args ) {
-        Popcorn popcorn = new Popcorn();
+        Popcorn popcorn = new Popcorn()
         {
-            void doSome () {
+            void doSome() {
             System.out.println("main");
         }
-        }
+        };
         popcorn.doSome();
+        popcorn.secondMethhod();
     }
 }
