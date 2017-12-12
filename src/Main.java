ef63da1d1ced5c34553e50eae99dd7e6947beccc
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-12 10:51:07 +0300

    Java_Lessons-89 Java 60: Reflection - рефлексия https://youtu.be/7eYQPNNxLSM?t=9m26s SomeClass SomeClass arg0 java.lang.String
    Process finished with exit code 0
    
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,7 +7,17 @@ public class Main {
         Class clss = someClass.getClass();
         Class clss2 = SomeClass.class;
         Class clss3 = Class.forName("SomeClass");
+        SomeClass someClass1 = (SomeClass) clss.newInstance();
 
+        Constructor[] constructors = clss.getDeclaredConstructors();
+        for (Constructor constructor : constructors) {
+            System.out.println(constructor.getName());
+            Parameter[] parameters = constructor.getParameters();
+            for (Parameter parameter : parameters) {
+                System.out.println(parameter.getName());
+                System.out.println(parameter.getType().getName());
+            }
+        }
     }
 }
 
