16881d08493456e7b9ebdb34e36b195ce38def93
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-12 10:34:45 +0300

    Java_Lessons-89 Java 60: Reflection - рефлексия https://youtu.be/7eYQPNNxLSM?t=5m20s 3 основных способа получения класса
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
+        SomeClass someClass = new SomeClass();
+        Class clss = someClass.getClass();
+        Class clss2 = SomeClass.class;
+        Class clss3 = Class.forName("SomeClass");
+
+    }
+}
+
+
+class SomeClass {
+    private static transient int i;
+    String s;
+
+    public SomeClass() {
+    }
+
+    SomeClass(String s) {
+        this.s = s;
+    }
+
+    public String someMethod(String s) {
+        System.out.println("this is" + s);
+        return s;
     }
 }
