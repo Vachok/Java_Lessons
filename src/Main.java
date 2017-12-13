1fc4d8484b90cd3f168581c42777df8942638d39
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 23:53:45 +0300

    Process finished with exit code 0 https://youtu.be/3NBnNbxkbjo thread main thread 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+
+    public static void main( String[] args ) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        System.out.println("thread main");
+    }
+}
+
+
+
+class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.print("thread 0");
     }
 }
