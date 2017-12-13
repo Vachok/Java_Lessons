a87fc4544747c684848e2379ca9d434fdb494f73
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 23:21:58 +0300

    3 threads https://youtu.be/bjh1HWV9RRk?t=15m15s https://gist.github.com/Vachok/a46484a0677378d8ea7c75fb9e80c154
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@
 public class Main {
 
     public static void main( String[] args ) {
-        MyThread myThread = new MyThread();
-        myThread.start();
-        myThread.start();
+        new MyThread().start();
+        new MyThread().start();
+        new MyThread().start();
     }
 }
 
@@ -15,9 +15,8 @@ class MyThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
-//        for (int i = 0; i < 400; i++) {
-//            System.out.println("Thread name is " + Thread.currentThread().getName() + " i = " + i);
-//        }
+        for (int i = 0; i < 400; i++) {
+            System.out.println("Thread name is " + Thread.currentThread().getName() + " i = " + i);
+        }
     }
 }
