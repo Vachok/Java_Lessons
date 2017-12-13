e9efc4118fe532b4ce69192add29d76439fdf27b
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 23:04:56 +0300

    main https://youtu.be/bjh1HWV9RRk?t=6m30s Exception in thread "Thread-0" java.lang.RuntimeException Thread-0 	at MyThread.domeMethod(Main.java:25) 	at MyThread.run(Main.java:20) this is new thread Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main( String[] args ) {
         MyThread myThread = new MyThread();
         myThread.start();
+        System.out.println(Thread.currentThread().getName());
     }
 }
 
@@ -14,6 +15,7 @@ class MyThread extends Thread {
 
     @Override
     public void run() {
+        System.out.println(Thread.currentThread().getName());
         System.out.print("this is new thread");
         domeMethod();
     }
