83e35697f7f4c4bd6024a1be8a839a0e0c7f0253
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 23:13:50 +0300

    Process finished with exit code 0 https://youtu.be/bjh1HWV9RRk?t=11m54s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main( String[] args ) {
         MyThread myThread = new MyThread();
         myThread.start();
-        System.out.println(Thread.currentThread().getName());
-        MyRunnable myRunnable = new MyRunnable();
-        Thread thread2 = new Thread(myRunnable);
-        thread2.start();
     }
 }
 
@@ -18,16 +14,8 @@ class MyThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
-    }
-}
-
-
-
-class MyRunnable implements Runnable {
-
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 400; i++) {
+            System.out.println("Thread name is " + Thread.currentThread().getName() + "i = " + i);
+        }
     }
 }
