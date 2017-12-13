c3cecb2cf554b1388630080a6690e0a5bc8465ae
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 23:18:02 +0300

    Exception in thread "Thread-0" java.lang.RuntimeException https://youtu.be/bjh1HWV9RRk?t=14m14s 	at MyThread.run(Main.java:21)
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main( String[] args ) {
         MyThread myThread = new MyThread();
-        myThread.run();
-        // myThread.start();
+        myThread.start(); // вызов потока (run - метод в потоке)
+        myThread.start();
     }
 }
 
