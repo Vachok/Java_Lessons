2cf95fedefa64594c57331e812688102142efb4d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-13 16:02:55 +0300

    Process finished with exit code 1 https://youtu.be/fG39TVmLX-c?t=1m28s Exception in thread "main" java.lang.NullPointerException 	at Main.main(Main.java:9)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.Console;
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args) {
+        Console console = System.console();
+        String s = "";
+        while(!s.equals("exit")){
+            s = console.readLine();
+            System.out.println(s);
+        }
     }
 }
