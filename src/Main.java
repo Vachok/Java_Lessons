3f1213c9519e7ca456844d1c6c19b87002f84998
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 11:37:53 +0300

    Process finished with exit code 0 https://youtu.be/3NBnNbxkbjo?t=14m55s thread main thread 0

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,18 @@
 public class Main {
 
    public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        myThread.start();
-      Thread.yield();
-        System.out.println("thread main");
-    }
+      MyThread myThread = new MyThread();
+      myThread.start();
+      System.out.println("thread main");
+   }
 }
 
 
 
 class MyThread extends Thread {
 
-    @Override
-    public void run() {
-       System.out.println("thread 0");
-    }
+   @Override
+   public void run() {
+      System.out.println("thread 0");
+   }
 }
