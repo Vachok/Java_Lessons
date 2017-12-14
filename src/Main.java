4c17224c1ed72848d8c9e737ff97a5975ad3f456
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 11:40:53 +0300

    Гарантированно отдаём время другому потоку https://youtu.be/3NBnNbxkbjo

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
    public static void main(String[] args) {
       MyThread myThread = new MyThread();
       myThread.start();
+      try{
+         myThread.join();
+      }
+      catch(InterruptedException e){
+         e.printStackTrace();
+      }
       System.out.println("thread main");
    }
 }
@@ -15,6 +21,14 @@ class MyThread extends Thread {
 
    @Override
    public void run() {
-      System.out.println("thread 0");
+      for(int i = 0; i < 50; i++){
+         try{
+            Thread.sleep(100);
+         }
+         catch(InterruptedException e){
+            e.printStackTrace();
+         }
+         System.out.println(i);
+      }
    }
 }
