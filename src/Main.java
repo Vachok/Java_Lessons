53de18edbbe74ad1c90ea26c2659b9a2feb1a608
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 12:16:58 +0300

    Java_Lessons-107 Java 68: Многопоточность 3: Синхронизация 107 https://youtu.be/GgRaiVF0sM0?t=4m4s

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args ) throws Exception {
+        Resource resource = new Resource();
+        resource.i = 5;
+        MyThread myThread = new MyThread();
+        MyThread myThread2 = new MyThread();
+        myThread.setResource(resource);
+        myThread2.setResource(resource);
+        myThread.start();
+        myThread2.start();
+        myThread.join();
+        myThread2.join();
+        System.out.println(resource.i);
+    }
+}
+
+class MyThread extends Thread {
+    Resource resource;
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+    @Override
+    public void run() {
+    }
+}
+
+class Resource {
+    int i;
+
+    public void changeI() {
+        int i = this.i;
+        i++;
+        this.i = i;
     }
 }
