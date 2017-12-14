dbe504d80a3e61c2d8db1e9a076065f80541ade8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 22:52:12 +0300

    Java_Lessons-108 Java 69: Многопоточность 4: синхронизация статический методов https://youtu.be/jz0pVqBgQCc
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import javax.annotation.Resource;
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+
+    public static void main( String[] args ) {
+        Resource resource = new Resource();
+        resource.setI(5);
+        MyThread mythread = new MyThread();
+    }
+}
+
+
+
+class MyThread extends Thread {
+
+    Resource resource;
+
+
+    @Override
+    public void run() {
+        resource.changeI();
+    }
+}
+
+
+
+class Resource {
+
+    private int i;
+
+
+    public int getI() {
+        return i;
+    }
+
+
+    public synchronized void setI( int i ) {
+        this.i = i;
+    }
+
+
+    public synchronized void changeI() {
+        int i = this.i;
     }
 }
