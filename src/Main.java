3a34aeeeb883fad17eede684e4ab016e818ccb90
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-14 12:29:27 +0300

    решение проблемы доступа к ресурсу https://youtu.be/GgRaiVF0sM0?t=10m4s

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ class MyThread extends Thread {
 class Resource {
     int i;
 
-    public void changeI() {
+    public synchronized void changeI() {
         int i = this.i;
         if(Thread.currentThread().getName().equals("one")){
             Thread.yield();
