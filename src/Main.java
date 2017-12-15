03f5c9021d3a2546ae3fd3702bfdee348689f3e1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-15 20:50:30 +0300

    Process finished with exit code 0 https://youtu.be/Z9U90-yzbgY?t=5m49s null first
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,37 @@ import java.util.List;
 
 public class Main {
 
-    List list = Collections.synchronizedList(new ArrayList<>());
 
     public static void main( String[] args ) {
+        NameList nameList = new NameList();
+        nameList.add("first");
+        class MyThread extends Thread {
 
+            @Override
+            public void run() {
+                System.out.println(nameList.removeFirst());
+            }
+        }
+        new MyThread().start();
+        new MyThread().start();
+    }
+
+
+    static class NameList {
+
+        private List list = Collections.synchronizedList(new ArrayList<>());
+
+
+        public void add( String name ) {
+            list.add(name);
+        }
+
+
+        public String removeFirst() {
+            if (list.size() > 0) {
+                return (String) list.remove(0);
+            }
+            return null;
+        }
     }
 }
