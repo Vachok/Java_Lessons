dedb9a2d228f65081ae0f8c3ccd9c318f5fd59e1
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-15 17:24:38 +0300

    9999 https://youtu.be/pHl6m4CIKj4?t=2m3s Process finished with exit code 0
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,23 @@
 
 public class Main {
 
-    // all non atmic!
-    int i;
-    Integer I;
-    double d;
-    Double D;
+    static int i;
 
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
+        for (int j = 0; j < 10000; j++) {
+            new MyThread().start();
+        }
+        Thread.sleep(2000);
+        System.out.println(i);
+    }
+
+
+    static class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            Main.i++;
+        }
     }
 }
