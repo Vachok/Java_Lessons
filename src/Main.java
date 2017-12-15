113e18f1daca56df5d55f7e217ed392a94b27ba8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-15 19:36:30 +0300

    117Java 72: Многопоточность 7: Синхронизированные коллекции https://youtu.be/Z9U90-yzbgY?t=1m1s
    Signed-off-by: Vachok <33656095+Vachok@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-
+// Java concurency package
+// https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/package-summary.html
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+
+    public static void main( String[] args ) {
     }
 }
