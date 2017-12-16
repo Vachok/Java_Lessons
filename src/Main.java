4f6a8b279d146e9bb92327867a30889fa2606564
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-17 00:06:35 +0300

    Exception in thread "Thread-0" https://youtu.be/_DcbJ3eYULw?t=7m59s Exception in thread "Thread-1" java.lang.NullPointerException

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,42 @@
 public class Main {
 
     public static void main( String[] args ) {
+        ResourceA resourceA = new ResourceA();
+        ResourceB resourceB = new ResourceB();
+        MyThread1 myThread1 = new MyThread1();
+        MyThread2 myThread2 = new MyThread2();
+        myThread1.resourceA = resourceA;
+        myThread2.resourceB = resourceA;
+        myThread1.start();
+        myThread2.start();
     }
 }
 
 
+
+class MyThread1 extends Thread {
+
+    ResourceA resourceA;
+
+
+    @Override
+    public void run() {
+        System.out.println(resourceA.getI());
+    }
+}
+
+
+
+class MyThread2 extends Thread {
+
+    ResourceA resourceB;
+
+
+    @Override
+    public void run() {
+        System.out.println(resourceB.getI());
+    }
+}
 
 class ResourceA {
 
@@ -14,6 +46,7 @@ class ResourceA {
 
 
     public synchronized int getI() {
+
         return resourceB.returnI();
     }
 
