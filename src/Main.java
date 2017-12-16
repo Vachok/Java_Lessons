06b08b8bfa8a6e80a4a2476b16427b4d2dea12b0
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-16 23:58:48 +0300

    Java_Lessons-118 Java 73: Многопоточность 8: Deadlock

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
 
 
 public class Main {
-    public static void main( String[] args ) throws Exception {
+
+    public static void main( String[] args ) {
+    }
+}
+
+
+
+class ResourceA {
+
+    ResourceB resourceB;
+
+
+    public synchronized int getI() {
+        return resourceB.returnI();
+    }
+
+
+    public synchronized int returnI() {
+        return 1;
+    }
+}
+
+
+
+class ResourceB {
+
+    ResourceA resourceA;
+
+
+    public synchronized int getI() {
+        return resourceA.returnI();
+    }
+
+
+    public synchronized int returnI() {
+        return 1;
     }
 }
