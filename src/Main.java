77b78f689dbb0070b37c9727a40f07275463ff9f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 00:12:18 +0300

    Java_Lessons-123 Java 76: Многопоточность 11: ReentrantLock https://youtu.be/2OnAxdxtNmw

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
 
 
+static class MyThread extends Thread {
+
+    Resource resource;
+
+
+    @Override
+    public void run() {
+        resource.changeI();
+    }
+}
+
+
+
 public class Main {
+
     public static void main( String[] args ) throws Exception {
+        Resource resource = new Resource();
+        resource.i = 5;
+        MyThread myThread = new MyThread();
+        myThread.setName("one");
+        MyThread myThread1 = new MyThread();
+        myThread.resource = resource;
+        myThread1.resource = resource;
+        myThread.start();
+        myThread1.start();
+        myThread.join();
+        myThread1.join();
+        System.out.println(resource.i);
+    }
+}
+
+
+
+class Resource {
+
+    int i = this.i;
+
+
+    void changeI() {
+        int i = this.i;
+        if (Thread.currentThread().getName().equals("one")) {
+            Thread.yield();
+        }
+        i++;
+        this.i = i;
     }
 }
