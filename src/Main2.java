486a43908724be13027ba278dd86dceab27f2463
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 11:37:14 +0300

    Exception in thread "Thread-1" https://youtu.be/UrubwFA3vA4?t=6m27s java.lang.IllegalMonitorStateException 	at java.base/java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.signal(AbstractQueuedSynchronizer.java:1962) 	at Main2$AccountPlus.run(Main2.java:19) account = 0 Exception in thread "Thread-0" java.lang.IllegalMonitorStateException 	at java.base/java.util.concurrent.locks.ReentrantLock$Sync.tryRelease(ReentrantLock.java:149) 	at java.base/java.util.concurrent.locks.AbstractQueuedSynchronizer.release(AbstractQueuedSynchronizer.java:1288) 	at java.base/java.util.concurrent.locks.AbstractQueuedSynchronizer.fullyRelease(AbstractQueuedSynchronizer.java:1747) 	at java.base/java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.await(AbstractQueuedSynchronizer.java:2059) 	at Main2$AccountMinus.run(Main2.java:29)
    Process finished with exit code 0

--- src/Main2.java
+++ src/Main2.java
@@ -5,22 +5,34 @@ import java.util.concurrent.locks.ReentrantLock;
 
 
 public class Main2 {
-   Lock lock = new ReentrantLock();
-   Condition condition = lock.newCondition();
+   static Lock lock = new ReentrantLock();
+   static Condition condition = lock.newCondition();
    static int account = 0;
    public static void main(String[] args) {
+      new AccountMinus().start();
+      new AccountPlus().start();
    }
    static class AccountPlus extends Thread {
       @Override
       public void run() {
          Main2.account += 10;
+         Main2.condition.signal();
       }
    }
 
    static class AccountMinus extends Thread {
       @Override
       public void run() {
-         System.out.println(Main2.account);
+         if(Main2.account < 10) {
+            try {
+               System.out.println("account = "+Main2.account);
+               Main2.condition.await();
+               System.out.println("account = "+Main2.account);
+            }
+            catch(InterruptedException e) {
+               e.printStackTrace();
+            }
+         }
          Main2.account -= 10;
       }
    }
