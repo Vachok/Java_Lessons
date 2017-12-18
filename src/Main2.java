c3683039be581bffe86c499cfa569b313918964a
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-18 11:28:35 +0300

    Java_Lessons-125 Jsva 78 : Многопоточность13: Conditions 125 https://youtu.be/UrubwFA3vA4?t=3m5s

--- src/Main2.java
+++ src/Main2.java
@@ -7,8 +7,22 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Main2 {
    Lock lock = new ReentrantLock();
    Condition condition = lock.newCondition();
+   static int account = 0;
    public static void main(String[] args) {
+   }
+   static class AccountPlus extends Thread {
+      @Override
+      public void run() {
+         Main2.account += 10;
+      }
+   }
 
+   static class AccountMinus extends Thread {
+      @Override
+      public void run() {
+         System.out.println(Main2.account);
+         Main2.account -= 10;
+      }
    }
 }
 // at 18.12.2017 (11:22)
