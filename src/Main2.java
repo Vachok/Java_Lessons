59f52eb893a36e082a7e04cd636204ae1fb218b8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-19 15:07:40 +0300

    CyclicBarier 138 https://youtu.be/mHkvGWks_YQ?t=33s

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.CyclicBarrier;
+
+
+
+public class Main2 {
+   public static void main(String[] args) {
+      CyclicBarrier cyclicBarrier = new CyclicBarrier();
+   }
+}
+// at 19.12.2017 (15:06)
