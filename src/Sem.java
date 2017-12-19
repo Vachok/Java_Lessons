208adc286b6a9aac5a492db1949b6017a6243096
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-19 13:11:54 +0300

    https://youtu.be/6dx3Ma4JeZE?t=4m18s

--- src/Sem.java
+++ src/Sem.java
@@ -1,5 +1,35 @@
+import java.util.concurrent.CountDownLatch;
+
+
+
 public class Sem {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException {
+      CountDownLatch countDownLatch = new CountDownLatch(3);
+      new Work(countDownLatch);
+      new Work(countDownLatch);
+      new Work(countDownLatch);
+      countDownLatch.await();
+      System.out.println("all done");
+   }
+}
+
+class Work extends Thread {
+   CountDownLatch countDownLatch;
+   public Work(CountDownLatch countDownLatch) {
+      this.countDownLatch = countDownLatch;
+      start();
+   }
+
+   @Override
+   public void run() {
+      try {
+         Thread.sleep(3000);
+      }
+      catch(InterruptedException e) {
+         e.printStackTrace();
+      }
+      System.out.println("done");
+      countDownLatch.countDown();
    }
 }
 // at 19.12.2017 (13:03)
