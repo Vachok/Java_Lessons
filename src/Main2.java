bad145dc12034200b9c99bbbcac1a1cb8afb71f3
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-22 23:47:06 +0300

    Java_Lessons-141 Java 87: Многопоточность 22: BlockingQueue https://youtu.be/vqPXhzD2bl0?t=3m39s почему-то не компилит

--- src/Main2.java
+++ src/Main2.java
@@ -1,25 +1,30 @@
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.PriorityBlockingQueue;
 
-public class Main2 {
-    public static void main(String[] args) {
-        BlockingDeque<String> queue = new PriorityBlockingQueue<>();
 
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println(queue.take());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+@SuppressWarnings ("ALL")
+public class Main2 {
+   public static void main(String[] args) {
+      
+      BlockingDeque<String> queue = new PriorityBlockingQueue<>();
+      
+      new Thread() {
+         @Override
+         public void run() {
+            
+            try {
+               System.out.println(queue.take());
+            } catch (InterruptedException e) {
+               e.printStackTrace();
             }
-        }.start();
-        new Thread() {
-            @Override
-            public void run() {
-                queue.add("this is string");
-            }
-        }.start();
-    }
+         }
+      }.start();
+      new Thread() {
+         @Override
+         public void run() {
+            
+            queue.add("this is string");
+         }
+      }.start();
+   }
 }
