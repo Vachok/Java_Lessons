bcded64abb15f2c6ecb5e55a92a88218858086d5
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-23 10:45:57 +0300

    Java_Lessons-143 Java 89: Многопоточность 24: ForkJoinFramework https://youtu.be/Ft8D_Toqa0k?t=2m01s https://goo.gl/aQrHx1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Date;
+
+
 public class Main {
-    static long numOfOperation = 10_000_000_000_000L;
+    static long numOfOperation = 10_000_000_000L;
     public static void main(String[] args) {
         
+        System.out.println(new Date());
         long j = 0;
         for(long i = 0;i < numOfOperation;i++){
-            j += i
-        } System.out.println(j);
+            j += i;
+        }
+        System.out.println(j);
+        System.out.println(new Date());
     }
 }
 
--- src/SumOfUsingForkJoin.java
+++ src/SumOfUsingForkJoin.java
@@ -3,5 +3,4 @@ public class SumOfUsingForkJoin {
 
 
 
-
-23.12.2017(10:38)by{Vachok}
+//23.12.2017(10:38)by{Vachok}
