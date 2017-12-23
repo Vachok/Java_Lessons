9c0dd106f8808ce5ed3e289cfff6f4aeb3edaef6
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-23 10:45:57 +0300

    Java_Lessons-143 Java 89: Многопоточность 24: ForkJoinFramework https://youtu.be/Ft8D_Toqa0k?t=2m01s https://goo.gl/aQrHx1
    (cherry picked from commit bcded64)

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,20 @@
-<<<<<<< HEAD
-import java.util.concurrent.ThreadFactory;
+import java.util.Date;
 
 
 public class Main {
+    static long numOfOperation = 10_000_000_000L;
     public static void main(String[] args) {
         
-        ThreadFactory threadFactory = new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                
-                Thread thread = new Thread(r);
-                thread.setPriority(Thread.MAX_PRIORITY);
-                return thread;
-            }
-        };
-        threadFactory.newThread(new MyRun()).start();
-    }
-    static class MyRun implements Runnable {
-        @Override
-        public void run() {
-            
-            System.out.println(1);
-        }
-=======
-public class Main {
-    static long numOfOperation = 10_000_000_000_000L;
-    public static void main(String[] args) {
-        
+        System.out.println(new Date());
         long j = 0;
         for(long i = 0;i < numOfOperation;i++){
-            j += i
-        } System.out.println(j);
->>>>>>> 3334a9f... Java_Lessons-143 Java 89: Многопоточность 24: ForkJoinFramework
+            j += i;
+        }
+        System.out.println(j);
+        System.out.println(new Date());
     }
 }
 
 
 
-<<<<<<< HEAD
-//23.12.2017(0:06)by{Vachok}
-=======
 //23.12.2017(10:39)by{Vachok}
->>>>>>> 3334a9f... Java_Lessons-143 Java 89: Многопоточность 24: ForkJoinFramework
