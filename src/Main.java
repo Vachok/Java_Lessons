96a35cc734876442fe0bc3107dda6ced28e93785
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-23 10:24:30 +0300

    Java_Lessons-142 Java 88: Многопоточность 23: ThreadFactory https://youtu.be/Bn4XARHucQI?t=2m1s

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.concurrent.ThreadFactory;
+
+
 public class Main {
     public static void main(String[] args) {
-    
+        
+        ThreadFactory threadFactory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                
+                Thread thread = new Thread(r);
+                thread.setPriority(Thread.MAX_PRIORITY);
+                return thread;
+            }
+        };
+    }
+    static class MyRun implements Runnable {
+        @Override
+        public void run() {
+            
+            System.out.println(1);
+        }
     }
 }
 
