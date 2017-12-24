1d093eded86f4f70dd00e626a060c7f6fc46f568
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-24 11:16:16 +0300

    How it works https://youtu.be/aMc1wHveqGM?t=4m49s

--- Lessons/src/Main.java
+++ Lessons/src/Main.java
@@ -1,6 +1,23 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.concurrent.Callable;
+
+
 public class Main {
    public static void main(String[] args) {
-      //new Comparable<>() //нельзя делать копию интерфейсов!
+      
+      InvocationHandler handler = new MyProxy();
+      Comparable comparable;
+      Class[] classes = new Class[]{Comparable.class, Callable.class};
+      Object proxy = Proxy.newProxyInstance(null, classes, handler);
+   }
+   static class MyProxy implements InvocationHandler {
+      @Override
+      public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+         
+         return null
+      }
    }
 }
 
