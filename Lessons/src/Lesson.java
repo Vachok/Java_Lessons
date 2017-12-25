1c5e003b3007ea6c0dcbbdb5872710c3e7cbd2e8
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-25 14:18:03 +0300

    Основное с каментами

--- Lessons/src/Lesson.java
+++ Lessons/src/Lesson.java
@@ -14,43 +14,43 @@ public class Lesson {
    
    public static void main(String[] args) {
    
-      JFrame jFrame = getFrame();
-      jFrame.add(new MyComponet());
+      JFrame jFrame = getFrame(); // покажем методом фрейм
+      jFrame.add(new MyComponet()); // добавим в него контент
    }
    static class MyComponet extends JComponent{
    
       @Override
       protected void paintComponent(Graphics g) {
    
-         Font font = new Font("Courier", Font.BOLD, 20);
-         Graphics2D g2 = ( Graphics2D ) g;
-         g2.setFont(font);
-         g2.drawString("hello world! i'm KUDR and i'm SWINGer! ))", 20, 20);
-         java.awt.geom.Point2D p1 = new java.awt.geom.Point2D.Double(70, 70);
-         java.awt.geom.Point2D p2 = new java.awt.geom.Point2D.Double(170, 170);
-         Line2D l2 = new Line2D.Double(p1, p2);
-         g2.draw(l2);
-         Ellipse2D el = new Ellipse2D.Double(70, 70, 170, 170);
-         g2.setPaint(Color.BLUE);
-         g2.draw(el);
-         Rectangle2D r2 = new Rectangle2D.Double(70, 70, 170, 170);
-         g2.draw(r2);
-         try{URL url = new URL("http://i65.tinypic.com/16bcabo.jpg");
-            Image image = new ImageIcon(url).getImage();
-            g2.drawImage(image, 192, 50, null );
+         Font font = new Font("Courier", Font.BOLD, 20); // выбираем шрифт
+         Graphics2D g2 = ( Graphics2D ) g; // указываем способ отрисовки
+         g2.setFont(font); // передаём выбранный шрифт
+         g2.drawString("hello world! i'm KUDR and i'm SWINGer! ))", 20, 20); // вывод надписи
+         java.awt.geom.Point2D p1 = new java.awt.geom.Point2D.Double(70, 70); // создаём точку
+         java.awt.geom.Point2D p2 = new java.awt.geom.Point2D.Double(170, 170); // создаём точку
+         Line2D l2 = new Line2D.Double(p1, p2); // чем рисуем точку
+         g2.draw(l2); // рисуем точку
+         Ellipse2D el = new Ellipse2D.Double(70, 70, 170, 170); // создаём эллипс
+         g2.setPaint(Color.BLUE); // красим в синий
+         g2.draw(el); // рисуем эллипс
+         Rectangle2D r2 = new Rectangle2D.Double(70, 70, 170, 170); // создаём квадрат
+         g2.draw(r2); // рисуем квадрат
+         try{URL url = new URL("http://i65.tinypic.com/16bcabo.jpg"); // показываем путь до картинки
+            Image image = new ImageIcon(url).getImage(); // качаем картинку
+            g2.drawImage(image, 192, 50, null ); // покажем картинку
          }
          catch(MalformedURLException e){
             e.printStackTrace();
          }
       }
-   }
+   } // контент
    static JFrame getFrame(){
       JFrame jFrame = new JFrame(){};
-      jFrame.setVisible(true);
-      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Toolkit toolkit = Toolkit.getDefaultToolkit();
-      Dimension dimension = toolkit.getScreenSize();
-      jFrame.setBounds(dimension.width/2-250, dimension.height/2-250, 500, 300 );
-      return jFrame;
-   }
+      jFrame.setVisible(true); // делаем видимым
+      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // делаем возможность его закрыть
+      Toolkit toolkit = Toolkit.getDefaultToolkit(); // метод
+      Dimension dimension = toolkit.getScreenSize(); // получим им разрешение экрана
+      jFrame.setBounds(dimension.width/2-250, dimension.height/2-250, 500, 300 ); // сделаем рамку, по-параметрам
+      return jFrame; // отдадим рамку
+   } // фрейм
 }
