837b8a5346be563ec65487280f4382b76342341d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-25 13:15:41 +0300

    Java_Lessons-179 Java 97: Swing 2: JComponent 179 https://youtu.be/YjD90CJBWTo?t=10m54s https://i.imgur.com/JBJnzlC.jpg Тут рисуем линию

--- Lessons/src/Lesson.java
+++ Lessons/src/Lesson.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 
 
@@ -14,11 +15,12 @@ public class Lesson {
    
       @Override
       protected void paintComponent(Graphics g) {
-         Font font = new Font("Arial", Font.BOLD, 20);
+         Font font = new Font("Courier", Font.BOLD, 20);
          Graphics2D g2 = (Graphics2D)g;
          g2.setFont(font);
-         g2.drawString("hello world! i'm KUDR and i'm SWINGer! ))", 20, 20);
-         
+         g2.drawString("hello world! i'm KUDR and i'm SWINGer! ))", 20, 90);
+         Line2D l2 = new Line2D.Double(70, 170 ,90, 190);
+         g2.draw(l2);
       }
    }
    static JFrame getFrame(){
