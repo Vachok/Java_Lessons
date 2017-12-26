a5a6368828447b360747bd10ce514f41de5b32be
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-26 13:05:25 +0300

    Текст на кнопке https://i.imgur.com/tkgRtpF.gifv

--- Lessons/src/Main.java
+++ Lessons/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
       
       jFrame.add(jPanel);
       JButton jButton = new JButton(new MyAction());
+      jButton.setText("Submit");
       jPanel.add(jButton);
    }
    static class MyAction extends AbstractAction{
