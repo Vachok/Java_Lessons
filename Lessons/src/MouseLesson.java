811a8b65179665b86e0789cd309c587c15ed9860
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-26 17:02:50 +0300

    Отображение координат мыши. https://youtu.be/roL9eWiwHak?t=7m14s https://i.imgur.com/6gthfVI.gifv

--- Lessons/src/MouseLesson.java
+++ Lessons/src/MouseLesson.java
@@ -10,18 +10,31 @@ import java.util.jar.JarFile;
 public class MouseLesson {
    
    static JFrame jFrame = MyJFrame.getFrame();
-   static JPanel jPanel = new JPanel();
+   
    public static void main(String[] args) {
       
-      jFrame.add(jPanel);
-      jPanel.addMouseListener(new MouseAdapter() {
-   
-   
+      JComponent jComponent = new MyComponent();
+      jFrame.add(jComponent);
+      jFrame.addMouseMotionListener(new MouseAdapter() { // следим за действиями мыши в jFrame
          @Override
-         public void mouseClicked(MouseEvent e) {
-            super.mouseClicked(e);
-            MouseLesson.jPanel.setBackground(Color.BLUE);
+         public void mouseMoved(MouseEvent e) { // будем следить за движениями мыши
+            super.mouseMoved(e);
+            MyComponent.xCoord = e.getX(); // берет Х
+            MyComponent.yCoord = e.getY(); // берёт Y
+            jComponent.repaint();
          }
       });
+   }
+   static class MyComponent extends JComponent {
+      
+      public static int xCoord;
+      public static int yCoord;
+      @Override
+      protected void paintComponent(Graphics g) {
+         
+         super.paintComponent(g);
+         g.drawString("Coordinates x: " + MyComponent.xCoord + " y: " + MyComponent.yCoord, 35, 35); // выведем на экран координаты
+         
+      }
    }
 }
