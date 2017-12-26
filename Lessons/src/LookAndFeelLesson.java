4194a1808f9d8f5be3aa21a573424b8e52765cdb
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-26 10:05:17 +0300

    Java_Lessons-182 Java 100: Swing 5: LookAndFeel 182 https://youtu.be/eoX8j_y10MU?t=1m33s Комп рабочий https://goo.gl/EG4ydf

--- Lessons/src/LookAndFeelLesson.java
+++ Lessons/src/LookAndFeelLesson.java
@@ -3,12 +3,13 @@ import java.awt.*;
 
 
 
-public class SingleClassLesson {
-   
+public class LookAndFeelLesson {
    public static void main(String[] args) {
-      JFrame jFrame = getFrame();
-      JPanel jPanel = new JPanel();
-      jFrame.add(jPanel);
+      UIManager.LookAndFeelInfo[] lookAndFeelInfos = UIManager.getInstalledLookAndFeels();
+      for(UIManager.LookAndFeelInfo lookAndFeelInfo : lookAndFeelInfos){
+         System.out.println(lookAndFeelInfo.getName());
+         System.out.println(lookAndFeelInfo.getClassName());
+      }
    }
    static JFrame getFrame(){
       JFrame jFrame = new JFrame(){};
@@ -19,4 +20,5 @@ public class SingleClassLesson {
       jFrame.setBounds(dimension.width/2-250, dimension.height/2-250, 500, 300 );
       return jFrame;
    } // тот самый фрейм
+   
 }
