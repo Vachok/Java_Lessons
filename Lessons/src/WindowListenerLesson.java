19806e0b9bf99e16cc97b4f8b95bf9c737eaa7f2
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-26 11:31:09 +0300

    Через WindowAdapter можно не прописывать все методы, а  только нужные

--- Lessons/src/WindowListenerLesson.java
+++ Lessons/src/WindowListenerLesson.java
@@ -10,47 +10,27 @@ public class WindowListenerLesson {
    
    public static void main(String[] args) {
       JFrame jFrame = getFrame();
-      jFrame.addWindowListener(new WindowListener() {
+      jFrame.addWindowListener(new WindowAdapter() {
    
    
-         @Override
-         public void windowOpened(WindowEvent e) {
-      
-         }
          @Override
          public void windowClosing(WindowEvent e) {
-      
+            int i = 0;
          }
-         @Override
-         public void windowClosed(WindowEvent e) {
+      });
+   }
       
-         }
-         @Override
-         public void windowIconified(WindowEvent e) {
-      
-         }
-         @Override
-         public void windowDeiconified(WindowEvent e) {
-      
-         }
-         @Override
-         public void windowActivated(WindowEvent e) {
-      
-         }
-         @Override
-         public void windowDeactivated(WindowEvent e) {
-      
-         }
-      })
+      static JFrame getFrame () {
+         JFrame jFrame = new JFrame() {
+         
+         
+         };
+         jFrame.setVisible(true);
+         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         Toolkit toolkit = Toolkit.getDefaultToolkit();
+         Dimension dimension = toolkit.getScreenSize();
+         jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 250, 500, 300);
+         return jFrame;
+      } // тот самый фрейм
    
-      static JFrame getFrame(){
-      JFrame jFrame = new JFrame(){};
-      jFrame.setVisible(true);
-      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Toolkit toolkit = Toolkit.getDefaultToolkit();
-      Dimension dimension = toolkit.getScreenSize();
-      jFrame.setBounds(dimension.width/2-250, dimension.height/2-250, 500, 300 );
-      return jFrame;
-   } // тот самый фрейм
-   
-}
+   }
