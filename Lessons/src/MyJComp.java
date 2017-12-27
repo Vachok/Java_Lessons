2e814de79a3ea00ec3102babbb7aca8397b2a06e
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-27 14:18:23 +0300

    fix():

--- Lessons/src/MyJComp.java
+++ Lessons/src/MyJComp.java
@@ -8,7 +8,7 @@ public class MyJComp {
    /** Нарисованный заранее JFrame
     *
     * @param size true/false. Если false - размер по-умолчанию
-    *                      x = dimension.width / 2 - 250;
+   x = dimension.width / 2 - 250;
    y = dimension.height / 2 - 250;
    width = 500;
    height = 300;
@@ -19,8 +19,11 @@ public class MyJComp {
     * @return JFrame, по заданным (true) или по-дефолтным (false) параметрам
     */
    static JFrame getFrame(boolean size, int x, int y, int width, int height) {
+   
+      JFrame jFrame = new JFrame() {
       
-      JFrame jFrame = new JFrame(){};
+      
+      };
       jFrame.setVisible(true);
       jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -33,4 +36,5 @@ public class MyJComp {
       }
       jFrame.setBounds(x, y, width, height);
       return jFrame;
-   } // тот самый фрейм
+   }// тот самый фрейм
+}
