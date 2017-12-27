9db9be91f85f1d98caad014785063e893e8fa099
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-27 14:59:06 +0300

    Создаём границы с названием

--- Lessons/src/Main.java
+++ Lessons/src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 
@@ -9,17 +10,20 @@ public class Main extends MyJComp {
    static JPanel jPanel = new JPanel();
    public static void main(String[] args) {
    
+      jFrame.setTitle("by Vachok");
       jFrame.add(jPanel);
-      jPanel.add(new JCheckBox("check"));
-      JRadioButton jRadioButton1 = new JRadioButton("one");
-      JRadioButton jRadioButton2 = new JRadioButton("two");
+      jPanel.add(new JCheckBox("18 марта 2018. TrueStory"));
+      JRadioButton jRadioButton1 = new JRadioButton("Путин");
+      JRadioButton jRadioButton2 = new JRadioButton("ХУЙ");
       jRadioButton2.setSelected(true);
       ButtonGroup buttonGroup = new ButtonGroup();
       buttonGroup.add(jRadioButton1);
       buttonGroup.add(jRadioButton2);
       jPanel.add(jRadioButton1);
       jPanel.add(jRadioButton2);
-      
+      Border border = BorderFactory.createEtchedBorder();
+      Border border1 = BorderFactory.createTitledBorder(border, "у тебя есть выБОР");
+      jPanel.setBorder(border1);
    }
    
 }
