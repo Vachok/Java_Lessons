5fe71f95b229d4c34f1c4fbe0233bec3e9bedcbb
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-27 12:02:56 +0300

    docs():

--- Lessons/src/MyJFrame.java
+++ Lessons/src/MyJFrame.java
@@ -5,6 +5,19 @@ import java.awt.*;
 
 public class MyJFrame {
    
+   /** Нарисованный заранее JFrame
+    *
+    * @param size true/false. Если false - размер по-умолчанию
+    *                      x = dimension.width / 2 - 250;
+   y = dimension.height / 2 - 250;
+   width = 500;
+   height = 300;
+    * @param x начальная точка по-горизонтали
+    * @param y начальная точка по-вертикали
+    * @param width ширина
+    * @param height высота
+    * @return JFrame, по заданным (true) или по-дефолтным (false) параметрам
+    */
    static JFrame getFrame(boolean size, int x, int y, int width, int height) {
       
       JFrame jFrame = new JFrame(){};
