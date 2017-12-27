e380a7e82231e5acc513e713e09c89f451671890
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-27 15:07:49 +0300

    Добавим выпадающее меню. border обязательно ставим ниже! https://youtu.be/0QUZ137wE5k?t=8m30s

--- Lessons/src/Main.java
+++ Lessons/src/Main.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class Main extends MyJComp {
    
-   static JFrame jFrame = getFrame(false, 0, 0, 0, 0);
+   static JFrame jFrame = getFrame(true, 500, 500, 800, 300);
    static JPanel jPanel = new JPanel();
    public static void main(String[] args) {
    
@@ -23,6 +23,11 @@ public class Main extends MyJComp {
       jPanel.add(jRadioButton2);
       Border border = BorderFactory.createEtchedBorder();
       Border border1 = BorderFactory.createTitledBorder(border, "у тебя есть выБОР");
+      JComboBox<String> comboBox = new JComboBox<>();
+      comboBox.addItem("свобода");
+      comboBox.addItem("равенство");
+      comboBox.addItem("УПЯЧКА");
+      jPanel.add(comboBox);
       jPanel.setBorder(border1);
    }
    
