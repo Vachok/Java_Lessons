e260bb58348ae7ad32024a7742746a72ff48e626
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-27 12:14:22 +0300

    Reset (#211)
    * Resettmp (#193)
    
    * Revert "Initial commit"
    
    * Java_Lessons-166 91: Dynamic Proxy - динамические прокси.
    
    * settings. background upd from remote
    
    * Доп. Отрисованный JFrame, JPannel
    
    * Отдельный класс MyJFrame
    
    * Метод getFrameWithButton
    
    * Тут доработал свой "тот самый фрейм"
    
    К уроку имеет косвенное отношение.
    Сделал фрейм, с возможностью задать размер и проверкой true/false.
    если брать размер по-дефолту, можно передать false, и нули.
    
    (cherry picked from commit 3e045c0)
    
    * docs():
    
    * Переделал класс. Для добавления своих методов с J-компонентами. (#208)
    
    * Revert "Переделал класс. Для добавления своих методов с J-компонентами. (#208)" (#209)
    
    This reverts commit c3178f8f4d51b9ab40656ad2d939d4fb6b146edb.
    
    * Переделал класс. Для добавления своих методов с J-компонентами. (#210)

---
+++ Lessons/src/MyJFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class MyJComp {
+   
+   /** Нарисованный заранее JFrame
+    *
+    * @param size true/false. Если false - размер по-умолчанию
+    *                      x = dimension.width / 2 - 250;
+   y = dimension.height / 2 - 250;
+   width = 500;
+   height = 300;
+    * @param x начальная точка по-горизонтали
+    * @param y начальная точка по-вертикали
+    * @param width ширина
+    * @param height высота
+    * @return JFrame, по заданным (true) или по-дефолтным (false) параметрам
+    */
+   static JFrame getFrame(boolean size, int x, int y, int width, int height) {
+      
+      JFrame jFrame = new JFrame(){};
+      jFrame.setVisible(true);
+      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Toolkit toolkit = Toolkit.getDefaultToolkit();
+      Dimension dimension = toolkit.getScreenSize();
+      if(!size) {
+         x = dimension.width / 2 - 250;
+         y = dimension.height / 2 - 250;
+         width = 500;
+         height = 300;
+      }
+      jFrame.setBounds(x, y, width, height);
+      return jFrame;
+   } // тот самый фрейм
