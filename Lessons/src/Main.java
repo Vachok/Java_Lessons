85df2b7d57b3d9a3c6aaea762a4870ba5de7868d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-28 11:44:23 +0300

    Меняем кнопкам layout https://i.imgur.com/VYLr6ot.jpg

--- Lessons/src/Main.java
+++ Lessons/src/Main.java
@@ -1,18 +1,19 @@
 import javax.swing.*;
-import java.util.jar.JarFile;
+import java.awt.*;
 
 
 
 public class Main {
-   
    static JFrame jFrame = MyJComp.getFrame(false, 0, 0, 0, 0);
+   
    public static void main(String[] args) {
       
-      JPanel jPanel = new JPanel();
-      jFrame.add(jPanel);
-      jPanel.add(new JButton("one"));
-      jPanel.add(new JButton("two"));
-      jPanel.add(new JButton("three"));
+      jFrame.add(new JButton("1"), BorderLayout.CENTER);
+      jFrame.add(new JButton("2"), BorderLayout.NORTH);
+      jFrame.add(new JButton("3"), BorderLayout.SOUTH);
+      jFrame.add(new JButton("4"), BorderLayout.WEST);
+      jFrame.add(new JButton("5"), BorderLayout.EAST);
+   
    }
    
 }
