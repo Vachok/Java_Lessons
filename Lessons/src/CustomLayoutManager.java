68edb4690c9bdd2f0974a1e98599649198d9df3d
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-28 13:06:53 +0300

    ниже и уже

--- Lessons/src/CustomLayoutManager.java
+++ Lessons/src/CustomLayoutManager.java
@@ -13,7 +13,7 @@ public class CustomLayoutManager extends MyJComp {
       jPanel.setLayout(null);
       
       JButton jButton = new JButton("Submit");
-      jButton.setBounds(150, 50, 150, 40);
+      jButton.setBounds(50, 150, 90, 40);
       jPanel.add(jButton);
    }
 }
