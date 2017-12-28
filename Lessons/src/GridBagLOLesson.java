fff04d6076e8a9887276d1517aa6a561c2932ce7
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-28 12:04:38 +0300

    Добавим на панель текстовое поле https://i.imgur.com/OpJps3Z.jpg

--- Lessons/src/GridBagLOLesson.java
+++ Lessons/src/GridBagLOLesson.java
@@ -12,7 +12,7 @@ public class GridBagLOLesson extends MyJComp {
       
       jFrame.add(jPanel);
       GridBagLayout gridBagLayout = new GridBagLayout();
-      jPanel.add(gridBagLayout);
+      jPanel.setLayout(gridBagLayout);
       GridBagConstraints constraints1 = new GridBagConstraints();
       constraints1.weightx = 0;
       constraints1.weighty = 0;
@@ -20,5 +20,6 @@ public class GridBagLOLesson extends MyJComp {
       constraints1.gridy = 0;
       constraints1.gridheight = 2;
       constraints1.gridwidth = 2;
+      jPanel.add(new TextArea(4, 20), constraints1);
    }
 }
