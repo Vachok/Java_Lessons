976d132731079b56797a96815fdab799e7934e20
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-28 12:02:34 +0300

    Layout "в клеточку" https://youtu.be/m_yDZscCnmQ?t=4m42s

---
+++ Lessons/src/GridBagLOLesson.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.jar.JarFile;
+
+
+
+public class GridBagLOLesson extends MyJComp {
+   
+   static JFrame jFrame = getFrame(false, 0, 0, 0, 0);
+   static JPanel jPanel = new JPanel();
+   public static void main(String[] args) {
+      
+      jFrame.add(jPanel);
+      GridBagLayout gridBagLayout = new GridBagLayout();
+      jPanel.add(gridBagLayout);
+      GridBagConstraints constraints1 = new GridBagConstraints();
+      constraints1.weightx = 0;
+      constraints1.weighty = 0;
+      constraints1.gridx = 0;
+      constraints1.gridy = 0;
+      constraints1.gridheight = 2;
+      constraints1.gridwidth = 2;
+   }
+}
