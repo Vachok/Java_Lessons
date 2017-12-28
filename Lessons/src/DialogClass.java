001c19e917af14548f943835c8997ed6fcfbc237
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-28 15:00:30 +0300

    Диалог MESSAGE

---
+++ Lessons/src/DialogClass.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class DialogClass extends MyJComp {
+   
+   static JFrame jFrame = getFrame(false, 0, 0, 0, 0);
+   static JPanel jPanel = new JPanel();
+   
+   public static void main(String[] args) {
+      
+      jFrame.add(jPanel);
+      JButton jButton = new JButton("Show Dialog");
+      jPanel.add(jButton);
+      jButton.addActionListener(new ActionListener() {
+         
+         
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            
+            JOptionPane.showMessageDialog(DialogClass.jPanel, "My Dia");
+         }
+      });
+      
+   }
+}
