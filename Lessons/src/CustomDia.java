a650f2d12a527fd2292415d9fe0af97b5ff68c4f
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-29 10:19:55 +0300

    Java_Lessons-218 118: Swing 23: Custom dialog 218 https://youtu.be/rmK4oLxcLwA?t=3m58s

--- Lessons/src/CustomDia.java
+++ Lessons/src/CustomDia.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 
@@ -13,6 +15,16 @@ public class CustomDia {
    public static void main(String[] args) {
       
       jFrame.add(jPanel);
+      JButton jButton = new JButton("Show Dialog");
+      JPanel.add(jButton);
+      jButton.addActionListener(new ActionListener() {
+      
+      
+         @Override
+         public void actionPerformed(ActionEvent e) {
+         
+         }
+      });
       
    }
    static class MyDia extends JDialog { // Создаём класс-расширение
