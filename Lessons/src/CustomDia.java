947538ff30f0b8146d96735add1fecf2d586f98c
Vachok <33656095+Vachok@users.noreply.github.com>
2017-12-29 09:46:37 +0300

    Создадим конструктор диалогов. https://youtu.be/rmK4oLxcLwA?t=3m16s

---
+++ Lessons/src/CustomDia.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class CustomDia {
+   
+   static JFrame jFrame = MyJComp.getFrame(false, 0, 0, 0, 0);
+   static JPanel jPanel = new JPanel();
+   // 29.12.2017 9:40
+   
+   
+   public static void main(String[] args) {
+      
+      jFrame.add(jPanel);
+      
+   }
+   static class MyDia extends JDialog {
+      
+      public MyDia() {
+         
+         super(CustomDia.jFrame, "Title", true);
+         add(new JTextField(), BorderLayout.NORTH);
+         add(new JButton("click"), BorderLayout.SOUTH);
+         setBounds(50, 50, 150, 50);
+      }
+   } // создание конструктора диалогов. Тот самый конструктор
+}
