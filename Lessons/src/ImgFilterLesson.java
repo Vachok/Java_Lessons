ea0f6d2d62fb5dcc4a6aaa6ccee3d99bff70b21e
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-09 16:11:44 +0300

    feat(Image filter): трансформаций много
    google!
    это концепт
    
    BREAKING CHANGE: https://youtu.be/CceBPDqenMs?t=5m20s

--- Lessons/src/ImgFilterLesson.java
+++ Lessons/src/ImgFilterLesson.java
@@ -15,10 +15,10 @@ public class ImgFilterLesson {
       File file = new File("C:\\Users\\ikudryashov.EATMEAT\\IdeaProjects\\Java_Lessons\\Lessons\\smile.jpg");
       BufferedImage image = ImageIO.read(file);
    
-      BufferedImage imageFiltered = new BufferedImage(image.getWidth(), image.getHeight(), image.getType()); // создаём новый пустой имадж
-      AffineTransform affineTransform = AffineTransform.getRotateInstance(45, image.getWidth() / 2, image.getHeight() / 2); // создание трансформации
-      BufferedImageOp op = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR); // создали операцию
-      op.filter(image, imageFiltered); // применили
+      BufferedImage imageFiltered = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+      AffineTransform affineTransform = AffineTransform.getRotateInstance(45, image.getWidth() / 2, image.getHeight() / 2);
+      BufferedImageOp op = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
+      op.filter(image, imageFiltered);
    
       ImageIO.write(imageFiltered, "png", new File("smiler.png"));
    }
