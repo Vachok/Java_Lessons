cd232e08161ff133cfc66106c0f1d10166b9eb35
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-09 16:44:44 +0300

    feat(Печать документов): Doc DocPrintJob
    Создаём документ, для вывода.
    Передаём параметры.
    Если документ больше 0 - делаем задание для печати.

--- Lessons/src/Main243.java
+++ Lessons/src/Main243.java
@@ -1,15 +1,21 @@
-import javax.print.DocFlavor;
-import javax.print.PrintService;
-import javax.print.PrintServiceLookup;
+import javax.print.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 
 public class Main243 {
    
-   public static void main(String[] args) {
+   public static void main(String[] args) throws PrintException {
       
       DocFlavor flavor = DocFlavor.INPUT_STREAM.JPEG;
-      PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, null)
+      PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, null);
+      InputStream inputStream = new FileInputStream("smile.jpg");
+      Doc doc = new SimpleDoc(inputStream, flavor, null);
+      if(services.length > 0) {
+         DocPrintJob job = services[0].createPrintJob();
+         job.print(doc, null);
+      }
    }
 }
 // at 09.01.2018 (16:34)
