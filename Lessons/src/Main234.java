ecbc0f6e2774fa337e06c719013d0fa135a9fd0f
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-09 10:40:48 +0300

    feat(Ellipse2D): fill
    Так можно зарисовать текстурой
    
    BREAKING CHANGE: https://youtu.be/cuuzm1BRg-I?t=2m31s

--- Lessons/src/Main234.java
+++ Lessons/src/Main234.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
 
 
 
@@ -18,6 +22,12 @@ public class Main234 {
          
          Graphics2D g2 = ( Graphics2D ) g;
          g2.setPaint(new GradientPaint(new Point(0, 0), Color.RED, new Point(200, 100), Color.YELLOW));
+         try{
+            g2.setPaint(new TexturePaint(ImageIO.read(new File("pexels-photo-220634.jpeg")), new Rectangle2D.Double(0, 0, 200, 100)));
+         }
+         catch(IOException e){
+            e.printStackTrace();
+         }
          Ellipse2D ellipse2D = new Ellipse2D.Double(0, 0, 200, 100);
          g2.fill(ellipse2D);
       }
