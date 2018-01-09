759b9f9e31d7223e56973280ec6702c09bae747e
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-09 15:05:16 +0300

    feat(Работа с изображениями): IO.write
    png - появляется. Но в нём ничего не изменилось.
    
    BREAKING CHANGE: https://youtu.be/VXINldZlatk?t=5m48s

--- Lessons/src/ImgRaiserLesson.java
+++ Lessons/src/ImgRaiserLesson.java
@@ -12,11 +12,15 @@ public class ImgRaiserLesson {
       
       File file = new File("C:\\Users\\ikudryashov.EATMEAT\\IdeaProjects\\Java_Lessons\\Lessons\\smile.jpg");
       BufferedImage image = ImageIO.read(file);
-      
-      WritableRaster raster = image.getRaster(); // это набор пикселей
+   
+      WritableRaster raster = image.getRaster();
       for(int i = 0;i < raster.getHeight();i++){
-         int[] pixel = raster.getPixel(i, 0, new int[4]); // читаем по-пиксельно (new int = значения в rgb)
+         int[] pixel = raster.getPixel(i, 0, new int[4]);
+         pixel[0] = 255;
+         raster.setPixel(i, 0, pixel);
       }
+      image.setData(raster);
+      ImageIO.write(image, "png", new File("smiler.png"));
    }
 }
 // at 09.01.2018 (14:51)
