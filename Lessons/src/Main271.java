cbf43be7719ef97abe6175152cb64d53c76c8b15
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-19 14:41:41 +0300

    feat(Lessons): Logging
    C:\Users\ikudryashov.EATMEAT\IdeaProjects\Java_Lessons
    
    BREAKING CHANGE: https://youtu.be/e1Balqbin84?t=11m16s

---
+++ Lessons/src/Main271.java
@@ -0,0 +1,16 @@
+import java.util.logging.Logger;
+
+
+
+public class Main271 {
+   
+   private static final Logger logger = Logger.getGlobal();
+//   private static final Logger logger2 = Logger.getLogger(MyJComp.class.getName());
+   
+   public static void main(String[] args) {
+      
+      System.out.println(System.getProperty("java.util.logging.config.file"));
+//      logger.info("message");
+   }
+}
+// at 19.01.2018 (14:05)
