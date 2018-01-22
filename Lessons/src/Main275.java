7e9818b70fb0f8e11d9764b377cc059c91c16257
Vachok <33656095+Vachok@users.noreply.github.com>
2018-01-22 15:04:14 +0300

    docs(JavaDoc): Practies
    Как работать с JavaDoc
    
    BREAKING CHANGE: https://youtu.be/NDypr06h8HU?t=16m7s
    
    Closes #275

---
+++ Lessons/src/Main275.java
@@ -0,0 +1,50 @@
+/**
+ * Класс 275. Документы
+ */
+public class Main275 {
+   
+   /**
+    * @author Vachok
+    * @version 1.1
+    * @since 1.0
+    * Хранит мой ИНТ
+    */
+   int myInt;
+   
+   /**
+    * Точка начала.
+    * @param args значения командной <strong>строки</strong>
+    * <img src="doc-files" alt="NoRa"/>
+    */
+   public static void main(String[] args) {
+   
+   }
+   
+   
+   /**
+    * Это поле тупо отдаёт i
+    * @param i какой-то инт
+    * @param s какая-то строка
+    * @throws RuntimeException если что-то не так - срём
+    * @return и
+    */
+   int getMyInt(int i, String s) throws RuntimeException {
+      
+      return i;
+   }
+   /**
+    * @deprecated используй getMyInt()
+    */
+   void depr() {
+   
+   }
+   /**
+    * Это описание с линком {@link Other#otherMethod()}
+    * @see Other#otherMethod()
+    * @see "какой-то описаний"
+    */
+   void newM() {
+   
+   }
+}
+// at 22.01.2018 (14:34)
---
+++ Lessons/src/package-info.java
@@ -0,0 +1,10 @@
+public class Other {
+   
+   /**
+    * Другой метод
+    */
+   void otherMethod() {
+   
+   }
+}
+// at 22.01.2018 (14:55)
