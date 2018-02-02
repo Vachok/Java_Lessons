663af2722cbedc58d515435442fc047b1a95a064
Vachok <33656095+Vachok@users.noreply.github.com>
2018-02-02 10:34:30 +0300

    feat(JMX): Notification
    Основная концепция Notification MBeans Мы вызываем операцию, во время выполнения программы, и хотим
    от неё данные Нужно заэкстендить класс NotificationBroadcasterSupport, Переопределить метод
    getNotificationInfo и добавить вызов в нужный нам метод jmx.serv.Hello.sayHello - пример рабочего
    кода. jmx/notif/2018-02-02_10-24-59.gif https://photos.app.goo.gl/yFLLlJLuNHVpkW9n1
    
    BREAKING CHANGE: https://youtu.be/8JC4u88ghn8
    
    Closes #390

--- src/jmx/serv/Hello.java
+++ src/jmx/serv/Hello.java
@@ -2,12 +2,24 @@ package jmx.serv;
 
 
 
-public class Hello implements HelloMBean {
+import javax.management.AttributeChangeNotification;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+
+
+
+public class Hello extends NotificationBroadcasterSupport implements HelloMBean {
    
    private String name;
+   static int sequenceNumber = 0;
+   
    @Override
    public void sayHello() {
    
+      System.out.println("YAHOO");
+      Notification notification = new AttributeChangeNotification(this, Hello.sequenceNumber++, System.currentTimeMillis(), "YO!", "sequenceNumber", "int", Hello.sequenceNumber - 1, Hello.sequenceNumber);
+      sendNotification(notification);
    }
    @Override
    public int addIntegers(int a, int b) {
@@ -28,5 +40,14 @@ public class Hello implements HelloMBean {
    public void setName() {
       
       this.name = name;
+   }
+   @Override
+   public MBeanNotificationInfo[] getNotificationInfo() {
+      
+      String[] types = new String[]{AttributeChangeNotification.ATTRIBUTE_CHANGE};
+      String name = AttributeChangeNotification.class.getName();
+      String description = "An attr of MBean changed!";
+      MBeanNotificationInfo info = new MBeanNotificationInfo(types, name, description);
+      return new MBeanNotificationInfo[]{info};
    }
 }
