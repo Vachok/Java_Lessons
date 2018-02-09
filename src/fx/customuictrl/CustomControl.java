933776df98d7e44e0edd6c22624362f54af9ad4b
Vachok <33656095+Vachok@users.noreply.github.com>
2018-02-09 16:29:21 +0300

    feat(FX): Custom Control
    Создаем свои собственные элементы. Можно сделать двумя путями. Либо через FXML, либо через
    собственный класс, который будет экстендить какой-то стандартный control
    
    BREAKING CHANGE: https://youtu.be/xXsftCRhOfo
    
    Closes #443

---
+++ src/fx/customuictrl/CustomControl.java
@@ -0,0 +1,39 @@
+package fx.customuictrl;
+
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class CustomControl extends VBox {
+    @FXML
+    private TextField textField;
+
+    public CustomControl() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("custom_control.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        fxmlLoader.load();
+    }
+
+    public String getText() {
+        return textProperty().get();
+    }
+
+    public void setText(String value) {
+        textProperty().set(value);
+    }
+
+    public StringProperty textProperty() {
+        return textField.textProperty();
+    }
+
+    @FXML
+    protected void doSomething() {
+        System.out.println("The Button!");
+    }
+}
