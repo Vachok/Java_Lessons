eeba38fd073b43e9f1a7a455e73f3cc25bf24bd0
Vachok <33656095+Vachok@users.noreply.github.com>
2018-04-25 17:15:35 +0300

    feat(Lesson 523-269): Patterns 2
    Factory
    
    BREAKING CHANGE: http://lesson.vachok.ru/src/pattern2/index.html
    
    Closes #523

---
+++ src/pattern/factor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ <b>Patterns</b>
+ <p>
+ Урок 523-269. Классы типа Factory.
+ <a href='https://youtu.be/A4uNotdECBg'>Видео</a>, <a href='https://github.com/Vachok/Java_Lessons/issues/523'>GitHub</a>
+ */
+package pattern.factor;
