b5ed3916dfd6b775a1b1fc074b4e6a80cf371c58
Vachok <33656095+Vachok@users.noreply.github.com>
2018-04-25 17:20:33 +0300

    523 269 factory (#541)
    feat(Lesson 523-269): Patterns 2
    
    Factory
    
    BREAKING CHANGE: http://lesson.vachok.ru/src/pattern/index.html
    
    Closes #523

---
+++ src/pattern/package-info.java
@@ -0,0 +1,8 @@
+/**
+ <b>PATTERNS</b>
+ <p>
+ Уроки. <a href='https://www.youtube.com/playlist?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX'>YouTube-канал</a>
+ <a href='https://github.com/Vachok/Java_Lessons/projects/1'>GitHub Project with Issues</a>
+ */
+package pattern;
+
