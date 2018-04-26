1dea68984a825f325f7be07ad8bf4e5e76c240af
Vachok <33656095+Vachok@users.noreply.github.com>
2018-04-26 17:24:53 +0300

    Java_Lessons-526 Patterns 5: Singelton 526

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Java_Lessons
-by https://www.youtube.com/playlist?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX
+Toyota max speed = 180
+Audi max speed = 240
+T60GO! max speed = 40
+T52GO! max speed = 50
---
+++ src/pattern/factor/package-info.java
@@ -0,0 +1,6 @@
+/**
+ <a href="https://github.com/Vachok/Java_Lessons/issues/523" target="_blank">Фабрики по производству фабрик!</a>
+
+ <a href="https://youtu.be/A4uNotdECBg" target="_blank">Patterns 2: Factory</a>
+ */
+package pattern.factor;
