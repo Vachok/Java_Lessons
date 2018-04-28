261aa6df29647f1bc24d7ee49a5dbbbdefd3a2c9
Vachok <143500@gmail.com>
2018-04-28 20:02:39 +0300

    Java_Lessons-532 Patterns 11: Filter

---
+++ src/pattern/filt/FiltMain.java
@@ -0,0 +1,9 @@
+package pattern.filt;
+
+
+
+public class FiltMain {
+}
+
+
+//28.04.2018 (20:00) by {14350}
---
+++ src/pattern/filt/package-info.java
@@ -0,0 +1,5 @@
+/**
+ <a href="https://youtu.be/u_QTGD3Z9EU" target=_blank>Filter</a>
+
+ @since Created at: Wed Apr 25 10:12:18 MSK 2018 <b>|</b> Updated at: Sat Apr 28 19:59:48 MSK 2018 */
+package pattern.filt;
