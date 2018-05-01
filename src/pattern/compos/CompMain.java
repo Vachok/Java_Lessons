78f8980a5877e4346a0d61536cd9b1b2032e3774
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-01 09:07:45 +0300

    533 279 patt12 composite (#572)
    * feat(530-276): Patterns 10
    
    Bridge
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/bridg/package-summary.html
    
    Closes #529
    
    * feat(531-277): Patterns 10
    
    Bridge
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/bridg/package-summary.html
    
    Closes #531
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * feat(532-278): Pattern 11
    
    Filter
    
    * feat(533-279): Patterns 12
    
    Composite. Композитные объекты
    
    Closes #533

---
+++ src/pattern/compos/CompMain.java
@@ -0,0 +1,19 @@
+package pattern.compos;
+
+
+import java.util.Date;
+
+/**
+ * <b>Основной класс.</b>
+ * <p>
+ *Основные требования к объекту - "листья" не могут содержать детей,
+ * т.е. наследоваться.
+ *
+ * @author 14350
+ * @since 30.04.2018
+ * @see Folder
+ */
+public class CompMain {
+    public static void main(String[] args) {
+    }
+}
