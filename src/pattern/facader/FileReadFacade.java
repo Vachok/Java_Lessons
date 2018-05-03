415296be19f570aa4c16863b12c6982d17bbf3d1
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-03 13:09:58 +0300

    feat(534-280): Patterns 14
    Facade
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/facader/package-summary.html
    
    Closes #535

---
+++ src/pattern/facader/FileReadFacade.java
@@ -0,0 +1,29 @@
+package pattern.facader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Фасад для {@link FacaMain#main(String[])}
+ *
+ * @author IKudryashov
+ * @since 03.05.2018
+ */
+class FileReadFacade {
+    /**
+     * Example
+     *
+     * @param fileName имя файла
+     * @return example
+     */
+    String readFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        StringBuilder stringBuilder = new StringBuilder();
+        int j = 0;
+        while ((j = reader.read()) != -1) {
+            stringBuilder.append((char) j);
+        }
+        return String.valueOf(stringBuilder);
+    }
+}
---
+++ src/pattern/proto/ProtoMain.java
@@ -0,0 +1,45 @@
+package pattern.proto;
+
+import java.util.Objects;
+
+/**
+ * <b>Основной класс</b>
+ * <p>
+ * Прототип, это объект, который умеет клонировать сам себя.
+ *
+ * <b>Зачем?</b>
+ * Например, при создании большого объекта, постоянно обращаться к БД - не очень хорошая мысль.
+ * Тут на помощь и приходит клон.
+ * Сохраняем его в {@link Cash}, и работаем с ним оттуда.
+ *
+ * @since 27.04.2018 (20:42)
+ */
+public class ProtoMain implements Cloneable {
+
+    /**
+     * Склонируем {@link Student}
+     * <p>
+     * Помещает клон {@link Student} в {@link Cash}
+     * Затем достанем наш клон с помощью <code>{@link Student} student = cash.getStudent();</code>
+     * Тут прототипом является {@link Student}.
+     * <code>
+     * student.hashCode() = 959447386
+     * studentClone.hashCode() = 453211571</code>
+     *
+     * @param args null
+     */
+    public static void main(String[] args) {
+        Cash cash = new Cash();
+        cash.setStudent(new Student());
+        Student student = new Student();
+        Student studentClone = null;
+        try {
+            studentClone = cash.getStudent();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("student = " + student.hashCode());
+        System.out.println("studentClone = " + Objects.requireNonNull(studentClone).hashCode());
+    }
+}
