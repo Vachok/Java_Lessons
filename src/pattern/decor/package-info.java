37bfbfc8201241f02bbf1c6e420829105a617ad2
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-03 12:17:41 +0300

    535 281 pat14.facade (#574)
    * feat(530-276): Patterns 10
    
    Bridge
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/bridg/package-summary.html
    
    Closes #529
    
    * feat(531-277): Patterns 10
    
    Bridge
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/bridg/package-summary.html
    
    Closes #531
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * Java_Lessons-532 Patterns 11: Filter
    
    * feat(532-278): Pattern 11
    
    Filter
    
    * feat(533-279): Patterns 12
    
    Composite. Композитные объекты
    
    Closes #533
    
    * Java_Lessons-534 Pattern 13: Decorator
    
    * feat(534-280): Patterns 13
    
    Decorator
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/decor/package-summary.html
    
    Closes #534
    
    * feat(534-280): Patterns 14
    
    Facade
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/facader/package-summary.html
    
    Closes #535

--- src/pattern/decor/package-info.java
+++ src/pattern/decor/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <a href="https://github.com/Vachok/Java_Lessons/issues/534" target=_blank>Decorator</a>
  *
- * @version Updated at
+ * @version Updated at 03.05.2018 (11:38)
  * @since 01.05.2018 (9:10)
  */
 package pattern.decor;
