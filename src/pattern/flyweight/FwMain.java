bbda30b9b2061724a1a143739164d704cb819a72
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-03 13:16:28 +0300

    feat(534-280): Patterns 14
    Facade
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/facader/package-summary.html
    
    Closes #535

---
+++ src/pattern/flyweight/FwMain.java
@@ -0,0 +1,12 @@
+package pattern.flyweight;
+
+/**
+ * <b>Основной класс</b>
+ * <p>
+ *
+ * @author IKudryashov
+ * @since 03.05.2018
+ */
+public class FwMain {
+}
+// at 03.05.2018 (13:12)
---
+++ src/pattern/flyweight/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * * <a href="https://github.com/Vachok/Java_Lessons/issues/536" target=_blank>Flyweight</a>
+ *
+ * @version Thu May 03 12:23:34 MSK 2018
+ * @since Wed Apr 25 10:14:02 MSK 2018
+ */
+package pattern.flyweight;
