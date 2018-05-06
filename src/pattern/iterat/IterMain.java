1ab867de214ca3e266805cfc356dba368f3f542d
Vachok <143500@gmail.com>
2018-05-06 11:57:47 +0300

    Java_Lessons-543  287: Patterns 20: Iterator

---
+++ src/pattern/iterat/IterMain.java
@@ -0,0 +1,10 @@
+package pattern.iterat;
+
+/**
+ * <b>Основной класс</b>
+ *
+ * @author 14350
+ * @since 06.05.2018 (11:56)
+ */
+public class IterMain {
+}
---
+++ src/pattern/iterat/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <a href="https://github.com/Vachok/Java_Lessons/issues/543" target=_blank>Iterator</a>
+ *
+ * @version Sun May 06 11:56:07 MSK 2018
+ * @since Thu Apr 26 04:42:15 MSK 2018
+ */
+package pattern.iterat;
+
