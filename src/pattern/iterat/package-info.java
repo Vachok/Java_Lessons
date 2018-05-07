f8291e1c497d2c5168bd240686f2ef867db2208b
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-07 09:34:24 +0300

    docs():

--- src/pattern/iterat/package-info.java
+++ src/pattern/iterat/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <a href="https://github.com/Vachok/Java_Lessons/issues/543" target=_blank>Iterator</a>
  *
- * @version Sun May 06 11:56:07 MSK 2018
+ * @version Mon May 07 09:33:24 MSK 2018
  * @since Thu Apr 26 04:42:15 MSK 2018
  */
 package pattern.iterat;
