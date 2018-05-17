29207bfe7de0a6d7d345df35f3dd8b6b2b5da162
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-17 16:55:56 +0300

    546.282 pat23.obs nc (#587)
    Observable
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/observ/package-summary.html
    
    Closes #546

--- src/pattern/proxer/package-info.java
+++ src/pattern/proxer/package-info.java
@@ -1,6 +1,5 @@
 /**
  <a href="https://github.com/Vachok/Java_Lessons/issues/537" target=_blank>Proxy</a>
-
-
  @version Thu May 03 14:16:59 MSK 2018
  @since Wed Apr 25 10:14:29 MSK 2018 */
+package pattern.proxer;
