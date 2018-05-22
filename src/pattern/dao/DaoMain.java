d30e55aeefd08df8ea9f738063e8ef0cfa81356c
Vachok <143500@gmail.com>
2018-05-22 20:23:32 +0300

    feat(562-299): Patterns 32
    DAO
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/dao/package-summary.html
    
    Closes #562

---
+++ src/pattern/dao/DaoMain.java
@@ -0,0 +1,9 @@
+package pattern.dao;
+
+/**
+ * <b>Основной класс</b>.
+ *
+ * @since 22.05.2018 (20:17)
+ */
+class DaoMain {
+}
---
+++ src/pattern/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <a href="https://github.com/Vachok/Java_Lessons/issues/562" target=_blank>DAO</a>
+ *
+ * @version Tue May 22 20:16:20 MSK 2018
+ * @since Fri Apr 27 19:23:41 MSK 2018
+ */
+package pattern.dao;
+
