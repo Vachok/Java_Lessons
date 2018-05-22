a316afdf91cea2461cac8931944491f13d2abb64
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-22 20:09:50 +0300

    feat(561-298): Patterns 31 (#602)
    Composite Entry
    
    BREAKING CHANGE: http://lesson.vachok.ru/pattern/composentry/package-summary.html
    
    Closes #561

---
+++ src/pattern/composentry/DepObj2.java
@@ -0,0 +1,17 @@
+package pattern.composentry;
+
+/**
+ * Сервис, висящий в сети и отдающий что-то.
+ *
+ * @author IKudryashov
+ * @see ComposEntMain
+ * @since 22.05.2018 (12:52)
+ */
+class DepObj2 {
+    /**
+     * @return two
+     */
+    String getData() {
+        return "two";
+    }
+}
