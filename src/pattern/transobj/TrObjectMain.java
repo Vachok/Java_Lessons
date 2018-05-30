1a49837246f3a2f40a9b42597b7d624bc84a7507
Vachok <33656095+Vachok@users.noreply.github.com>
2018-05-30 13:16:02 +0300

    feat(566-303): Patterns 36
    Transfer Object
    
    http://lesson.vachok.ru/pattern/transobj/package-summary.html
    
    Closes #566

---
+++ src/pattern/transobj/TrObjectMain.java
@@ -0,0 +1,23 @@
+package pattern.transobj;
+
+/**
+ * <b>Основной класс</b>.
+ <p>
+ Концепция - всё собирается в один класс и его уже и выдёргиваем.
+ * @author IKudryashov
+ * @since 30.05.2018 (13:14)
+ */
+public class TrObjectMain {
+}
+
+/**
+ * Объект для трансфера.
+ * <p>
+ *
+ * @author IKudryashov
+ * @since 30.05.2018 (13:28)
+ */
+class User {
+    String uName;
+    int age;
+}
