681617280a126ab00acebb3c09622a3cac94c2c5
Vachok <33656095+Vachok@users.noreply.github.com>
2018-07-16 11:59:05 +0300

    feat(SOLID 4): Liskov principle
    Дети не должны менять параметры родителя.
    
    Closes #614

---
+++ src/solid/liskov/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <a href="https://github.com/Vachok/Java_Lessons/issues/614" target=_blank>Liskov Principle</a>
+ * Основная концепция - нельзя менять поведение класса-родителя,
+ * работа должна осуществля
+ *
+ * @since 01.06.2018 (16:41)
+ */
+package solid.liskov;
