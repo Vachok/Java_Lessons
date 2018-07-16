e49f2e36d39f1439eef8ccd159a041e85a2b5047
Vachok <33656095+Vachok@users.noreply.github.com>
2018-07-16 11:44:46 +0300

    feat(SOLID 4): Liskov principle
    Дети не должны менять параметры родителя.
    
    Closes #614

--- src/one/three/ThurdClass.java
+++
@@ -1,9 +0,0 @@
-package one.three;
-
-import one.TestClass;
-
-public class ThurdClass {
-    void method() { //todo https://youtu.be/z9R-iFfsSzs?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX&t=135
-        TestClass testClass = new TestClass();
-        System.out.println(testClass);
-}
