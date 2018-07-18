78325eaf36a05c36dc075ab8ee12eae83eb06ec5
Vachok <143500@gmail.com>
2018-07-18 15:06:10 +0300

    Java_Lessons-620 Java 313: Generic 4: Bounds

---
+++ generics/src/main/java/lessons/generics/bnds/genmethods/CellWithFace.java
@@ -0,0 +1,39 @@
+package lessons.generics.bnds.genmethods;
+
+
+import java.io.Serializable;
+
+/**  {@link Laun4}
+Можно расширить функционал интерфейсами.
+ Они добавляются через апостроф
+
+ @since 18.07.2018 (13:34)
+ @param <T>  */
+class CellWithFace<T extends Job & Serializable>{
+
+    /**
+     * Generic Object
+     */
+    T t;
+
+    /**
+     @param t {@link T}
+     */
+    void setT(T t) {
+        this.t = t;
+    }
+
+    /**{@link Job}
+     *
+     * Теперь объект T может выполнять метод {@link Job#doIt()}
+     */
+    public void doJob(){
+        t.doIt();
+    }
+
+    /** Можно создать Generic методы, которые хранят независимые друг от друга типы.
+     */
+    <E> E getE(E e){
+        return e;
+    }
+}
---
+++ generics/src/main/java/lessons/generics/bnds/genmethods/package-info.java
@@ -0,0 +1,5 @@
+/**<a href="https://github.com/Vachok/Java_Lessons/issues/620" target=_blank>Generic 4: Bounds</a>
+ {@link lessons.generics.genmethods.Laun3}
+ * @since 18.07.2018 (14:29)
+ */
+package lessons.generics.bnds.genmethods;
