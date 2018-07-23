efdba1bd2db765b2e1438738441c635dcb190ca8
Vachok <143500@gmail.com>
2018-07-23 17:33:57 +0300

    feat(630.317): Stack Queue Dequeue
    https://youtu.be/OVaaMUCpbF0?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX
    
    BREAKING CHANGE: http://lesson.vachok.ru/general/lessons/general/stackqdeq/package-summary.html
    
    Closes 630

--- general/src/main/java/lessons/general/alistllistone/ALLists.java
+++ general/src/main/java/lessons/general/alistllistone/ALLists.java
@@ -2,45 +2,47 @@ package lessons.general.alistllistone;
 
 import lessons.general.Lessons;
 import lessons.general.StartMe;
+import lessons.general.helper.SaveToDatabase;
+import lessons.general.helper.SaverProgress;
 
 import java.util.*;
-
 import java.util.concurrent.ConcurrentHashMap;
 
-/**Разница {@link java.util.ArrayList} и {@link java.util.LinkedList}
- <p>
- {@link ArrayList} принято использовать в общих случаях.
- <p>
- {@link LinkedList} ghbyznj использовать как очередь.
-
- {@link #theLesson()}
+/**
+ * Разница {@link java.util.ArrayList} и {@link java.util.LinkedList}
+ * <p>
+ * {@link ArrayList} принято использовать в общих случаях.
+ * <p>
+ * {@link LinkedList} принято использовать как очередь.
+ * <p>
+ * {@link #theLesson()}
+ *
  * @since 23.07.2018 (14:36)
-
  */
 public class ALLists implements Lessons {
-
-
+    private static Map<String, String> links = new ConcurrentHashMap<>();
+    private SaverProgress saverProgress = new SaveToDatabase();
     @Override
     public void launchMe() {
-        StartMe startMe = new StartMe(links, 623.316);
-        startMe.launchMe();
-        Map<String, String> links = new HashMap<>();
         links.put(this.getClass().getSimpleName(), "https://github.com/Vachok/Java_Lessons/issues/623");
         links.put("YouTube", "https://youtu.be/uSldKx6JVs0?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX");
+        /*theLesson();
+        System.out.println(" saved: "+saverProgress.isSaved(links, 623.316)+ " 24.07.2018 (16:37) ");*/
     }
+
     /**
      * Есть A и L листы. Оба имплементят {@link java.util.List} (доступ может быть осуществлён по-индексу.)
      * Все элементы индексируются и хранятся с индексами.
-     <p>
-     <b>ArrayList</b>. Хранит данные в простом массиве {@link Object}ов. {@link ArrayList#elementData}.
-     Позволяет пробегать по элементам. Работает быстро.
-     {@link #arrayWorks(ArrayList)}
-     <p>
-     <b>LinkedList</b>. Хранит данные нодами. one-two-three. Всегда есть last и first.
-     Элементы разбросаны в памяти, но между ними существуют ссылки. <i>Зачем?</i> В некоторых случаюх, ему требуется <b>гораздо</b>
-     меньше операций на запрос.
+     * <p>
+     * <b>ArrayList</b>. Хранит данные в простом массиве {@link Object}ов. {@link ArrayList#elementData}.
+     * Позволяет пробегать по элементам. Работает быстро.
+     * {@link #arrayWorks(ArrayList)}
+     * <p>
+     * <b>LinkedList</b>. Хранит данные нодами. one-two-three. Всегда есть last и first.
+     * Элементы разбросаны в памяти, но между ними существуют ссылки. <i>Зачем?</i> В некоторых случаюх, ему требуется <b>гораздо</b>
+     * меньше операций на запрос.
      */
-    private void theLesson(){
+    private void theLesson() {
         System.out.println("ALLists.launchMe");
         ArrayList<String> arrayList = new ArrayList<>();
         LinkedList<String> linkedList = new LinkedList<>();
@@ -61,45 +63,48 @@ public class ALLists implements Lessons {
 
         arrayWorks(arrayList);
         linkedWorks(linkedList);
+
     }
 
-    /**Особенности {@link LinkedList}
-     *
+    /**
+     * Особенности {@link LinkedList}
+     * <p>
      * -Удаление без смещения элементов. Потому что хранит ссылки на следующий-предыдущий элемент.
      *
      * @param linkedList {@link #launchMe()}
      */
     private void linkedWorks(LinkedList<String> linkedList) {
         long l = System.currentTimeMillis();
-        for (String s:linkedList){
+        for (String s : linkedList) {
             System.out.println("lnn = " + s);
         }
         linkedList.remove("three");
-        for (String s:linkedList){
+        for (String s : linkedList) {
             System.out.println("lnn = " + s);
         }
         System.out.println((System.currentTimeMillis() - l));
     }
 
-    /**Особенности {@link ArrayList}
-     *
+    /**
+     * Особенности {@link ArrayList}
+     * <p>
      * -Удаляет первый встреченный элемент.
-     * Посчитаем операции. 1-сверка, 2-сверка, 3-сверка!-попал-удалил, 5-сместил, 6-сместил. Итого - 6 операций.
+     * Посчитаем операции. 1-сверка, 2-сверка, 3-сверка!-попал-удалил, 5-сместил, 6-сместил. В итоге - 6 операций.
      * 6 элементов - 6 операций.
      * При удалении по-индексу <b>не будет операции пробегания сначала!</b> Т.е. в данном примере будет 6-2 операции.
      * Потому что {@link ArrayList} implements {@link java.util.RandomAccess}, и берёт элемент сразу, без перебора.
-     <p>
-     Основная концепция. Если работать с индексами - {@link ArrayList} будет быстрее.
-
+     * <p>
+     * Основная концепция. Если работать с индексами - {@link ArrayList} будет быстрее.
+     *
      * @param arrayList {@link #launchMe()}
      */
     private void arrayWorks(ArrayList<String> arrayList) {
         long l = System.currentTimeMillis();
-        for (String s:arrayList){
+        for (String s : arrayList) {
             System.out.println("arr = " + s);
         }
         arrayList.remove("three");
-        for (String s:arrayList){
+        for (String s : arrayList) {
             System.out.println("arr = " + s);
         }
         System.out.println((System.currentTimeMillis() - l));
---
+++ general/src/main/java/lessons/general/stackqdeq/package-info.java
@@ -0,0 +1,6 @@
+/**<a href="https://github.com/Vachok/Java_Lessons/issues/630" target=_blank>Stack and Queue</a>
+ *
+ * @since 24.07.2018 (16:40)
+
+ */
+package lessons.general.stackqdeq;
