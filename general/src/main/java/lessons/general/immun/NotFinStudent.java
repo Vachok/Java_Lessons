56f81daa737dd239b0a4900b193e9641aa04ad6d
Vachok <33656095+Vachok@users.noreply.github.com>
2018-08-02 17:14:28 +0300

    feat(638.323):  Immutable классы (#650)
    https://youtu.be/2PPPW6I-C34
    
    BREAKING CHANGE: http://lesson.vachok.ru/general/lessons/general/immun/package-summary.html
    
    Closes 638

---
+++ general/src/main/java/lessons/general/immun/NotFinStudent.java
@@ -0,0 +1,19 @@
+package lessons.general.immun;
+
+import java.util.Date;
+
+/**@deprecated
+ * @since 02.08.2018 (17:02)
+ */
+@Deprecated
+class NotFinStudent /*extends Student*/{
+
+    public NotFinStudent(int age, String name, Date date) {
+        /*super(age, name, date);*/
+    }
+
+
+    public Date getDate1() {
+        return new Date();
+    }
+}
