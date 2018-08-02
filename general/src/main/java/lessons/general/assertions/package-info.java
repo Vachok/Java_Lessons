f0a84c8b1bfe121b40970fa1ad04a9b3144c50ab
Vachok <33656095+Vachok@users.noreply.github.com>
2018-08-02 13:17:08 +0300

    feat(635.322): Assertions (#649)
    https://youtu.be/VCY_oxXa6PU
    
    BREAKING CHANGE: --
    
    Closes 634

---
+++ general/src/main/java/lessons/general/assertions/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <a href="https://github.com/Vachok/Java_Lessons/issues/637" target=_blank>Assertions</a>
+ *
+ * @see lessons.general.assertions.AssEr
+ * @since 02.08.2018 (13:16)
+ */
+package lessons.general.assertions;
