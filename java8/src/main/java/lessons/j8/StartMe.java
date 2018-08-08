587ece61c8932e7d025cc4d682817c431094aeb5
Vachok <143500@gmail.com>
2018-08-08 11:48:30 +0300

    feat(660.335): Как работают лямбда выражения.
    https://youtu.be/GMzP5vIG9YU
    
    BREAKING CHANGE: http://lesson.vachok.ru/j8/lessons/j8/lamdhowworks/LamHowItWorks.html
    
    Closes 660

--- java8/src/main/java/lessons/j8/StartMe.java
+++ java8/src/main/java/lessons/j8/StartMe.java
@@ -4,6 +4,7 @@ import lessons.j8.colle.Collie;
 import lessons.j8.helper.SaveToDatabase;
 import lessons.j8.helper.SaveToFile;
 import lessons.j8.helper.SaverProgress;
+import lessons.j8.lamdhowworks.LamHowItWorks;
 import lessons.j8.locdate.LocDateClass;
 import lessons.j8.methref.MethRef;
 import lessons.j8.opti.Option;
@@ -19,7 +20,7 @@ import java.util.Properties;
 public class StartMe implements Lessons {
     private static final String APP_NAME =  "u0466446_lessons-";
     private static final String SOURCE_CLASS = StartMe.class.getSimpleName();
-    private static Lessons lessons = new LocDateClass();
+    private static Lessons lessons = new LamHowItWorks();
     private static Properties properties = new Properties();
     private static InitProperties initProperties = new DBRegProperties(StartMe.class.getPackageName() +
         "-" + SOURCE_CLASS);
---
+++ java8/src/main/java/lessons/j8/lamdhowworks/package-info.java
@@ -0,0 +1,5 @@
+/**<b>Как работают лямбда выражения</b>
+ * {@link lessons.j8.lamdhowworks.LamHowItWorks}
+ * @since 08.08.2018 (11:13)
+ */
+package lessons.j8.lamdhowworks;
