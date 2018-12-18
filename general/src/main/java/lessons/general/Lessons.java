4b51518527617fc115e902f7d89e97938c791061
Vachok <143500@gmail.com>
2018-12-18 13:55:57 +0300

    Java_Lessons-682 Урок Java 344: Сложность алгоритмов поиска по коллекции

--- general/src/main/java/lessons/general/Lessons.java
+++ general/src/main/java/lessons/general/Lessons.java
@@ -1,12 +1,42 @@
 package lessons.general;
 
 
+import lessons.general.helper.SaveToDatabase;
+import lessons.general.helper.SaveToFile;
+import lessons.general.helper.SaverProgress;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
 /**
  * Его должны имплементить уроки.
  */
 public interface Lessons {
 
-    void linksPut(String lessonName, String links, boolean saveToDB);
+    /**
+     * Сохранение прогресса.
+     *
+     * @param lessonName Название урока
+     * @param links      ссылка на <a href="https://www.youtube.com" target=_blank>youtube</a>
+     * @param saveToDB   true - созраняет в БД, false - в файл.
+     */
+    default void linksPut(String lessonName, String links, boolean saveToDB) {
+        SaverProgress saverProgress = new SaveToFile();
+        if (saveToDB) {
+            saverProgress = new SaveToDatabase();
+        }
+        Map<String, String> stringStringMap = new TreeMap<>();
+        stringStringMap.put(lessonName, links);
+        try{
+        saverProgress.isSaved(stringStringMap, new String(lessonName.getBytes(), "UTF-8"));
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        Logger.getLogger(this.getClass().getSimpleName()).warning(saverProgress.toString());
+    }
 
     /**
      * Загрузочный метод. Запуск класса.
