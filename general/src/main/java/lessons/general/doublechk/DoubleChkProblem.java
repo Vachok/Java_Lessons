e62e35570c78259b4aa78695088e3b045c92afda
Vachok <143500@gmail.com>
2018-12-18 15:57:26 +0300

    Java_Lessons-685 Урок Java 347: проблема double check

---
+++ general/src/main/java/lessons/general/doublechk/DoubleChkProblem.java
@@ -0,0 +1,16 @@
+package lessons.general.doublechk;
+
+import lessons.general.Lessons;
+
+/**
+ * Проблема double check #685
+ * <p>
+ *
+ * @since 18.12.2018 (15:52)
+ */
+public class DoubleChkProblem implements Lessons {
+    @Override
+    public void launchMe() {
+        linksPut("685-347", "https://youtu.be/5eTQKyK-rGs", true);
+    }
+}
