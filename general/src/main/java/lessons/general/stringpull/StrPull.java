81da32a0a45a00e94c1dbe842b9db395c14723c7
Vachok <143500@gmail.com>
2019-01-09 15:41:51 +0300

    Java_Lessons-694 Урок Java 350: Как обычную строку поместить в пул

---
+++ general/src/main/java/lessons/general/stringpull/StrPull.java
@@ -0,0 +1,16 @@
+package lessons.general.stringpull;
+
+import lessons.general.Lessons;
+
+/**
+ * Как обычную строку поместить в pull
+ * <p>
+ *
+ * @since 09.01.2019 (15:38)
+ */
+public class StrPull implements Lessons {
+    @Override
+    public void launchMe() {
+        linksPut("694-350", "https://youtu.be/1QP8y6-1fD0?list=PL786bPIlqEjRDXpAKYbzpdTaOYsWyjtCX", false);
+    }
+}
