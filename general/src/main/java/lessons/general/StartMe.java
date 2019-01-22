22e8e6a755e223a688ea13b439cd472266f70d6c
Vachok <143500@gmail.com>
2019-01-22 18:13:49 +0300

    Java_Lessons-696 Урок Java 352: Расспределение нагрузки

--- general/src/main/java/lessons/general/StartMe.java
+++ general/src/main/java/lessons/general/StartMe.java
@@ -1,5 +1,6 @@
 package lessons.general;
 
+import lessons.general.balancing.NewBalance;
 import lessons.general.concurentpack.ConcPack;
 import lessons.general.dataoncollections.DataOnCol;
 import lessons.general.doublechk.DoubleChkProblem;
@@ -24,7 +25,7 @@ import java.util.Map;
 import java.util.Properties;
 
 public class StartMe implements Lessons {
-    private static Lessons lessons = new OverHeap();
+    private static Lessons lessons = new NewBalance();
     private static final String APP_NAME =  "u0466446_lessons-";
     private static final String SOURCE_CLASS = StartMe.class.getSimpleName();
     private static Properties properties = new Properties();
---
+++ general/src/main/java/lessons/general/balancing/NewBalance.java
@@ -0,0 +1,16 @@
+package lessons.general.balancing;
+
+import lessons.general.Lessons;
+
+/**
+ * Распределение нагрузки
+ * <p>
+ *
+ * @since 22.01.2019 (18:12)
+ */
+public class NewBalance implements Lessons {
+    @Override
+    public void launchMe() {
+        linksPut("696-352", "https://youtu.be/h-yvep_icv4", true);
+    }
+}
